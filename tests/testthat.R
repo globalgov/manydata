bc6d56fe7e0a24b32c055a7a77be1b15c94af67d
James Hollway <james.hollway@graduateinstitute.ch>
2020-09-01 10:58:34 +0200

    Added tests for entitle()

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(qdatr)
+
+test_check("qdatr")
---
+++ tests/testthat/test-standardise.R
@@ -0,0 +1,7 @@
+test_that("capitalisation works",{
+  expect_match(entitle("A treaty to do things"), "A Treaty To Do Things")
+})
+
+test_that("white space is stripped",{
+  expect_match(entitle("A treaty to do things  "), "A Treaty To Do Things")
+})
