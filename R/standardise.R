d35157eed44782795040df45975e01919174a518
James Hollway <james.hollway@graduateinstitute.ch>
2020-09-28 13:23:44 +0200

    Closes #23 by adding resequence()

--- R/standardise.R
+++ R/standardise.R
@@ -49,3 +49,47 @@ entitle <- function(s, strict = FALSE) {
                           ignore.case = TRUE, fixed = FALSE)
   out
 }
+
+#' Resorting and filtering dates
+#'
+#' Resorting and filtering dates
+#' @param data a dataframe
+#' @param vars a character vector identifying columns in the dataframe to sequence
+#' @param unity a string identifying how multiple entries may be glued together.
+#' By default, tidyr::unite() glues using the underscore "_".
+#' @return a dataframe/columns
+#' @import lubridate
+#' @examples
+#' \dontrun{
+#' TODO
+#' }
+#' @export
+resequence <- function(data, vars, unity = "_"){
+
+  require(lubridate)
+
+  len <- length(vars)
+
+  out <- apply(data[,vars], 1, function(x) {
+    dates <- sort(unlist(strsplit(unique(na.omit(x)),unity)))
+
+    if (length(dates) < len){
+      dates <- interleave(dates, which(is.na(x)))
+    }
+
+    if (length(dates) > len){
+      if (sum((!grepl("-01-01", dates))*1)>=len) dates <- dates[!grepl("-01-01",dates)]
+      if (sum((!grepl("9999", dates))*1)>=len) dates <- dates[!grepl("9999",dates)]
+
+      dmax <- max(as.duration(interval(dates[1:(length(dates)-1)],dates[2:(length(dates))])))
+      dmax <- which(as.duration(interval(dates[1:(length(dates)-1)],dates[2:(length(dates))]))==as.duration(dmax))
+      dates <- dates[c(1,dmax+1)]
+    }
+
+    dates
+  })
+
+  t(out)
+
+}
+
