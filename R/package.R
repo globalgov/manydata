d37f6a7e7db0c25d869db855409754061df1955d
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-10-02 17:44:22 +0200

    Removed trailing white spaces and added spaces between curly braces on package script

--- R/package.R
+++ R/package.R
@@ -1,23 +1,26 @@
 #' Create a new package in the qDatr ecosystem
 #'
 #' Creates a new package in and consistent with the qDatr ecosystem
-#' @param packageName A string giving the desired name of the package, must start with "q"
-#' @param packageAuthor A string, list or vector giving the package author(s), required
-#' @param update A logical indicating whether existing files should be overwritten, by default TRUE.
+#' @param packageName A string giving the desired name of the package,
+#' must start with "q"
+#' @param packageAuthor A string, list or vector giving the package
+#' author(s), required
+#' @param update A logical indicating whether existing files should be
+#' overwritten, by default TRUE.
 #' TODO: do these checks internally and smartly...
-#' @details The function establishes many of the required files and folder structures
-#' required for a qDatr-consistent data package.
-#' @return A new package structure 
+#' @details The function establishes many of the required files and
+#' folder structures required for a qDatr-consistent data package.
+#' @return A new package structure
 #' @importFrom usethis create_tidy_package
 #' @examples
 #' \dontrun{
-#' qpackage_create("qStates", c("James Hollway", "Henrique Sposito"))
+#' qpackage_create("qStates",
+#' c("James Hollway", "Henrique Sposito"))
 #' }
 #' @export
-create_qpackage <- function(packageName, 
+create_qpackage <- function(packageName,
                             packageAuthor,
-                            update = TRUE){
-  
+                            update = TRUE) {
   if(is.null(packageName)) stop("Please declare a package name")
   if(!startsWith(packageName, "q")) stop("Package name must start with a 'q'")
   if(is.null(packageAuthor)) stop("Please declare at least one author")
@@ -37,6 +40,5 @@ create_qpackage <- function(packageName,
   # usethis::use_git()
   # usethis::use_tidy_github_actions()
   # usethis::use_tibble()
-  
 }
 
