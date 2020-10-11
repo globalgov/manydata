3c5ee1120addf41452b23eea96d6171cc7a4175d
James Hollway <james.hollway@graduateinstitute.ch>
2020-10-11 13:06:43 +0200

    First draft of data documentation template

--- inst/templates/qData-document.R
+++ inst/templates/qData-document.R
@@ -1,2 +1,12 @@
-# Qdata Template
-# This is a template for how to Collect, Correct and Connect Data.
+#' Write the citation here.
+#'
+#' This dataset contains ...
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"object"
