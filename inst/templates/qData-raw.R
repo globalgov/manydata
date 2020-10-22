79855bdae0b6fae083a2f2fe31fe19ee68fbad02
James Hollway <james.hollway@graduateinstitute.ch>
2020-10-22 16:55:50 +0200

    Used new details extracted from path to simplify preparation template

--- inst/templates/qData-raw.R
+++ inst/templates/qData-raw.R
@@ -5,10 +5,7 @@
 library(qDatr)
 
 # Stage one: collecting data
-# For example, you may wish to import a CSV file
-{{{name}}} <- read_csv("data-raw/data/dataset/{{{name}}}.csv")
-# For excel files you may wish to import with the read_excel() function and 
-# for dta files you may wish to import with read_stata () function.
+{{{name}}} <- {{{impcmd}}}("{{{path}}}")
 
 # Stage two: correcting data
 # In this stage you will want to correct the variable names and 
@@ -16,7 +13,7 @@ library(qDatr)
 # below (in stage three) passes all the tests. Several of the functions in 
 # the qDatr package fit the purpose of wrangling and standardising data. 
 # The function transmutate(), for example, returns mutated variables
-# while dropping the original varibles used in the mutation. 
+# while dropping the original variables used in the mutation. 
 # Please refer to package manual further details and examples.
 
 # Stage three: connecting data
