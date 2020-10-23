a7b653ff91b205df6479cc7b56aa1ca9c045f6f7
James Hollway <james.hollway@graduateinstitute.ch>
2020-10-23 13:56:44 +0200

    Renamed qData-raw to qData-prep to simplify reference

--- inst/templates/qData-prep.R
+++ inst/templates/qData-prep.R
@@ -1,15 +1,15 @@
-# {{{name}}} Preparation Script
+# {{{dataset}}} Preparation Script
 
 # This is a template for how to Collect, Correct and Connect data 
 # for the qDatr system.
 library(qDatr)
 
 # Stage one: collecting data
-{{{name}}} <- {{{impcmd}}}("{{{path}}}")
+{{{dataset}}} <- {{{import_type}}}("{{{path}}}")
 
 # Stage two: correcting data
 # In this stage you will want to correct the variable names and 
-# formats of the '{{{name}}}' object until the object created 
+# formats of the '{{{dataset}}}' object until the object created 
 # below (in stage three) passes all the tests. Several of the functions in 
 # the qDatr package fit the purpose of wrangling and standardising data. 
 # The function transmutate(), for example, returns mutated variables
@@ -19,8 +19,8 @@ library(qDatr)
 
 # Stage three: connecting data
 # At this stage, all you need to do is run the next line to 
-# make {{{name}}} available within the package.
-export_data({{{name}}})
+# make {{{dataset}}} available within the package.
+export_data({{{dataset}}})
 # Please note that export_data() also creates documentation and test files for data.
 # Please make sure to cite source dataset on documentation file created. 
 # Please make sure data complies with qDatr requirements by running test script created 
