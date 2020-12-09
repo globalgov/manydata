71ef2d7ed813046a72545db2fbcab1af9235b2f2
James Hollway <james.hollway@graduateinstitute.ch>
2020-12-09 19:37:57 +0100

    Updated preparation script to reflect listing changes in export_data()

--- inst/templates/qData-prep.R
+++ inst/templates/qData-prep.R
@@ -10,21 +10,22 @@ library(qData)
 # Stage two: Correcting data
 # In this stage you will want to correct the variable names and
 # formats of the '{{{dataset}}}' object until the object created
-# below (in stage three) passes all the tests. Several of the functions in
-# the qData package fit the purpose of wrangling and standardising data.
-# The function transmutate(), for example, returns mutated variables
-# while dropping the original variables used in the mutation.
-# Please refer to package manual further details and examples.
-# TODO: Get users to check whether missing observations are
-# correctly stated as NA.
+# below (in stage three) passes all the tests. 
+{{{dataset}}} <- as_tibble({{{dataset}}}) %>%
+  transmutate(ID = {id_variable_name_here},
+              Beg = standardise_dates({date_variable_name_here})) %>%
+  dplyr::arrange(Beg, ID)
+# qData includes several functions that should help cleaning and standardising your data.
+# Please see the vignettes or website for more details.
 
-# Stage three: connecting data
-# At this stage, all you need to do is run the next line to
-# make {{{dataset}}} available within the package.
-export_data({{{dataset}}})
-# Please note that export_data() also creates documentation and test
-# files for data.
-# Please make sure to cite source dataset on documentation file created.
-# Please make sure data complies with qData requirements by running
-# test script created and, if any of the tests fail, please go
-# back to step two and correct the data.
+# Stage three: Connecting data
+# Next run the following line to make {{{dataset}}} available within the qPackage.
+export_data({{{dataset}}}, database = "{{{database}}}")
+# This function also does two additional things.
+# First, it creates a set of tests for this object to ensure adherence to certain standards.
+# You can hit Cmd-Shift-T (Mac) or Ctrl-Shift-T (Windows) to run these tests locally at any point.
+# Any test failures should be pretty self-explanatory and may require you to return
+# to stage two and further clean, standardise, or wrangle your data into the expected format.
+# Second, it also creates a documentation file for you to fill in.
+# Please make sure that you cite any sources appropriately and fill in as much detail
+# about the variables etc as possible.
