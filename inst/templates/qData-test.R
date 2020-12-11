dbcc20276dffdcf47a360d7c83bff8ce0d8eb553
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-11 17:05:36 +0100

    Fixed spelling mistakes on qData-test template

--- inst/templates/qData-test.R
+++ inst/templates/qData-test.R
@@ -11,3 +11,16 @@ test_that("missing obsevarsions are reported correctly", {
   expect_length(grepl("n/a", {{{dat}}}), 0)
   expect_length(grepl("N/A", {{{dat}}}), 0)
 })
+
+test_that("dates are standardised", {
+  expect_col_is_date({{{dat}}}, vars(Beg))
+  expect_col_is_date({{{dat}}}, vars(End))
+  expect_length(grepl("/", {{{dat}}}), 0)
+})
+
+test_that("titles are standirdised", {
+  expect_length(grepl("U.S.", {{{dat}}}), 0)
+  expect_length(grepl("U.K.", {{{dat}}}), 0)
+  expect_length(grepl("?", {{{dat}}}), 0)
+  expect_length(grepl("NANA.", {{{dat}}}), 0)
+})
