ed8932c43f752b89ade8cf1df52e798ad037f6cc
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-14 14:37:31 +0100

    Updated test template to work with dataset within a database list

--- inst/templates/test_qtest.R
+++ inst/templates/test_qtest.R
@@ -1,26 +1,26 @@
 library(pointblank)
 
 test_that("object is correct", {
-  expect_col_exists({{{dab}}}[[{{{dat}}}]], vars(endsWith("ID")))
-  expect_col_exists({{{dab}}}[[{{{dat}}}]], vars(Beg))
-  expect_col_exists({{{dab}}}[[{{{dat}}}]], vars(End))
+  expect_col_exists({{{dab}}}[["{{{dat}}}"]], vars(endsWith("ID")))
+  expect_col_exists({{{dab}}}[["{{{dat}}}"]], vars(Beg))
+  expect_col_exists({{{dab}}}[["{{{dat}}}"]], vars(End))
 })
 
 test_that("missing obsevarsions are reported correctly", {
-  expect_length(grepl("-", {{{dab}}}[[{{{dat}}}]]), 0)
-  expect_length(grepl("n/a", {{{dab}}}[[{{{dat}}}]]), 0)
-  expect_length(grepl("N/A", {{{dab}}}[[{{{dat}}}]]), 0)
+  expect_length(grepl("-", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_length(grepl("n/a", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_length(grepl("N/A", {{{dab}}}[["{{{dat}}}"]]), 0)
 })
 
 test_that("dates are standardised", {
-  expect_col_is_date({{{dab}}}[[{{{dat}}}]], vars(Beg))
-  expect_col_is_date({{{dab}}}[[{{{dat}}}]], vars(End))
-  expect_length(grepl("/", {{{dab}}}[[{{{dat}}}]]), 0)
+  expect_col_is_date({{{dab}}}[["{{{dat}}}"]], vars(Beg))
+  expect_col_is_date({{{dab}}}[["{{{dat}}}"]], vars(End))
+  expect_length(grepl("/", {{{dab}}}[["{{{dat}}}"]]), 0)
 })
 
 test_that("labels are standardised", {
-  expect_length(grepl("U.S.", {{{dab}}}[[{{{dat}}}]]), 0)
-  expect_length(grepl("U.K.", {{{dab}}}[[{{{dat}}}]]), 0)
-  expect_length(grepl("?", {{{dab}}}[[{{{dat}}}]]), 0)
-  expect_length(grepl("NANA.", {{{dab}}}[[{{{dat}}}]]), 0)
+  expect_length(grepl("U.S.", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_length(grepl("U.K.", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_length(grepl("?", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_length(grepl("NANA.", {{{dab}}}[["{{{dat}}}"]]), 0)
 })
