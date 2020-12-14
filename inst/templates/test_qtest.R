457cd859372f8ccc8fac5515c8f47e4e0963f53f
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-14 19:24:48 +0100

    Updated test template

--- inst/templates/test_qtest.R
+++ inst/templates/test_qtest.R
@@ -7,20 +7,20 @@ test_that("object is correct", {
 })
 
 test_that("missing obsevarsions are reported correctly", {
-  expect_length(grepl("-", {{{dab}}}[["{{{dat}}}"]]), 0)
-  expect_length(grepl("n/a", {{{dab}}}[["{{{dat}}}"]]), 0)
-  expect_length(grepl("N/A", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_false(any(grepl("-", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("n/a", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("N/A", {{{dab}}}[["{{{dat}}}"]])))
 })
 
 test_that("dates are standardised", {
   expect_col_is_date({{{dab}}}[["{{{dat}}}"]], vars(Beg))
   expect_col_is_date({{{dab}}}[["{{{dat}}}"]], vars(End))
-  expect_length(grepl("/", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]])))
 })
 
 test_that("labels are standardised", {
-  expect_length(grepl("U.S.", {{{dab}}}[["{{{dat}}}"]]), 0)
-  expect_length(grepl("U.K.", {{{dab}}}[["{{{dat}}}"]]), 0)
-  expect_length(grepl("?", {{{dab}}}[["{{{dat}}}"]]), 0)
-  expect_length(grepl("NANA.", {{{dab}}}[["{{{dat}}}"]]), 0)
+  expect_false(any(grepl("U.S.", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("U.K.", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("?", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("NANA.", {{{dab}}}[["{{{dat}}}"]])))
 })
