99881e37bf28fd841d7aca47f1d393fafe7040da
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-14 15:01:25 +0100

    Deleted qData logo pdf file and script to generate hexlogo

--- inst/qData_hexlogo.R
+++
@@ -1,8 +0,0 @@
-library(hexSticker)
-library(pdftools)
-
-hexSticker::sticker("C:/Users/h-spo/Documents/GitHub/qData/inst/qDatalogo.pdf",
-                    p_size = 1, p_y = 1, s_x = 1, s_y = 1, s_width = 2, s_height = 2, 
-                    h_color = "white", h_size = 1, white_around_sticker = TRUE, dpi = 800, 
-                    package="",
-                    filename="C:/Users/h-spo/Documents/GitHub/qData/inst/qdata_hexlogo.png")
