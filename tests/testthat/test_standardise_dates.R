97404d0f1f4ef636311a837355d3cb1d04f14e36
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-15 11:59:40 +0100

    Updated tests for standardise_dates()

--- tests/testthat/test_standardise_dates.R
+++ tests/testthat/test_standardise_dates.R
@@ -2,9 +2,9 @@ test_that("standardise_dates() treats typical dates correctly",{
   expect_equal(standardise_dates("2010-01-01"), anytime::anydate("2010-01-01"))
 })    
 
-test_that("standardise_dates() treats reverse ordered dates correctly",{
-  expect_equal(standardise_dates("30.1.1874"), anytime::anydate("1874-01-30"))
-  expect_equal(standardise_dates("31.12.1879"), anytime::anydate("1879-12-31"))
+test_that("standardise_dates() treats reverse ordered dates correctly", {
+  expect_match(standardise_dates("30.1.1874"), anytime::anydate("1874-01-30"))
+  expect_match(standardise_dates("31.12.1879"), anytime::anydate("1879-12-31"))
   expect_match(standardise_dates("6/10/2012"), anytime::anydate("2012-06-20"))
   expect_match(standardise_dates("9.4.1939"), anytime::anydate("1939-04-09"))
   expect_match(standardise_dates("12/5/1993"), anytime::anydate("1993-05-12"))
