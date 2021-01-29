15ef4bc801adc4b8496504f20e259876db1d0f2d
James Hollway <james.hollway@graduateinstitute.ch>
2021-01-29 17:49:07 +0100

    Fixed 3/5 test_standardise_dates bugs

--- tests/testthat/test_standardise_dates.R
+++ tests/testthat/test_standardise_dates.R
@@ -80,20 +80,19 @@ test_that("standardise_dates() treats historical dates correctly",{
   expect_match(as.character(standardise_dates("44 BC")), "-044-01-01")
   expect_match(as.character(standardise_dates("-712")), "-712-01-01")
   expect_match(as.character(standardise_dates("-1712")), "-1712-01-01")
-  expect_match(as.character(standardise_dates("-1712-12-10")), "-1712-12-10")
-  expect_match(as.character(standardise_dates("-12-10-1712")), "-1712-12-10")
+  expect_match(as.character(standardise_dates("-1712-12-10")), "-1712-12-10") #tofix
+  expect_match(as.character(standardise_dates("-12-10-1712")), "-1712-12-10") #tofix
 })
 
-dat <- data.frame(date = as.character(c("2010.10.12","2010/03/30", "10/12/93", "12-10-1993", "NA")))
-dat1 <- data.frame(date = c("NA", "2010.10.12"," ", "10/12/93", "12-10-1993"))
-dat2 <- data.frame(date = c("2010.10.12", "2010/11/13", "2010-12-14"))
-dat3 <- data.frame(date = c("2010-10-12", "2010-11-13", "2010-12-14"))
-datr <- data.frame(date = as.Date(c("2010-10-12", "2010-11-13", "2010-12-14")))
-
 test_that("standardise_dates() treats multiple inconsistent dates correctly",{
-  expect_match(as.character(standardise_dates(c("2010-10-12", "2010-11-13", "2010-12-14"))), c("2010-10-12", "2010-11-13", "2010-12-14"))
-  expect_match(as.character(standardise_dates(dat3)), datr)
-  expect_match(as.character(standardise_dates(dat2)), c("2010-10-12", "2010-11-13", "2010-12-14"))
-  expect_match(as.character(standardise_dates(dat)), c("2010-10-12", "2010-03-30", "1993-10-12", "1993-10-12", "NA"))
-  expect_match(as.character(standardise_dates(dat1)), c("NA", "2010-10-12", "NA", "1993-10-12", "1993-10-12"))
+  
+  expect_equal(as.character(standardise_dates(c("2010-10-12", "2010-11-13", "2010-12-14"))), c("2010-10-12", "2010-11-13", "2010-12-14"))
+  dat3 <- data.frame(date = c("2010-10-12", "2010-11-13", "2010-12-14"))
+  expect_equal(as.character(standardise_dates(dat3$date)), c("2010-10-12", "2010-11-13", "2010-12-14"))
+  dat2 <- data.frame(date = c("2010.10.12", "2010/11/13", "2010-12-14"))
+  expect_equal(as.character(standardise_dates(dat2$date)), c("2010-10-12", "2010-11-13", "2010-12-14"))
+  dat <- data.frame(date = as.character(c("2010.10.12","2010/03/30", "10/12/93", "12-10-1993", "NA")))
+  expect_equal(as.character(standardise_dates(dat$date)), c("2010-10-12", "2010-03-30", "1993-10-12", "1993-10-12", NA)) #tofix
+  dat1 <- data.frame(date = c("NA", "2010.10.12", " ", "10/12/93", "12-10-1993"))
+  expect_equal(as.character(standardise_dates(dat1$date)), c(NA, "2010-10-12", NA, "1993-10-12", "1993-10-12")) #tofix
 })
