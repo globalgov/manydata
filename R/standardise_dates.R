e3d80203c3c2e85167b362066551915d774112e0
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-01 20:13:00 +0100

    Moved steps two and three of standardise_dates() into help functions

--- R/standardise_dates.R
+++ R/standardise_dates.R
@@ -31,6 +31,12 @@ standardise_dates <- standardize_dates <- function(...){
   # Step one: if necessary, join multiple columns
   dates <- parse_date_input(...)
   
+  # Step two: standardise inputs
+  dates <- standardise_date_input(dates)
+  
+  # Step three: correct date formats and sizes
+  dates <- correct_date_format(dates)
+  
 
 # Helper functions for standardise_dates()
 parse_date_input <- function(...){
@@ -45,8 +51,14 @@ parse_date_input <- function(...){
   dates
 }
 
+standardise_date_input <- function(dates){
   
-  # Second step: standardise inputs
+  as_bc_dates <- function(dates) {
+    dates <- stringr::str_remove_all(dates, "(bc|BC|Bc|bC)") # remove before christ letters
+    dates <- paste0("-", dates) # adds a negative sign to date
+    dates
+  }
+  
   dates <- as.character(dates) # makes sure dates are in character format
   dates <- stringr::str_replace_all (dates, "^NA$", "")
   dates <- stringr::str_replace_all(dates, "\\.", "-") # standardising separaters
@@ -59,8 +71,52 @@ parse_date_input <- function(...){
   dates <- stringr::str_remove_all(dates, "(ad|AD|Ad|aD)") # remove after christ
   dates <- ifelse(stringr::str_detect(dates, "(bc|BC|Bc|bC)"), as_bc_dates(dates), dates) # replacing BC for corresponding negative dates
   dates <- stringr::str_trim(dates, side = "both") # removes trailing white spaces
+}
+
+correct_date_format <- function(dates){
   
-  # Third step: correct date formats and sizes
+  incomp_dates <- function(dates) {
+    
+    thresh <- as.numeric(substr(Sys.Date(),1,4))
+    x <- matrix(as.numeric(unlist(strsplit(dates, "-"))), ncol=3, byrow = T)
+    ypos <- which(apply(x, 2, function(y) any(y>31) | any(nchar(y)==4))) # identify year
+    if(length(ypos)!=1) ypos <- 3
+    dpos <- setdiff(which(apply(x, 2, function(y) any(y>12) & all(y<32))), ypos) # identify day
+    if(length(dpos)==0) dpos <- setdiff(c(1,3), ypos)
+    mpos <- setdiff(setdiff(1:3, ypos), dpos) # identify month
+    
+    if(nrow(x)==1){
+      y <- ifelse(nchar(x[ypos])>2,
+                  ifelse(x[ypos] > thresh & x[ypos]!="9999",
+                         paste0("19", x[ypos] %% 100),
+                         formatC(x[ypos], width = 4, flag = "0")), # if 4, keep or correct
+                  ifelse(x[ypos] > (thresh %% 100),
+                         paste0("19", formatC(x[ypos], width = 2, flag = "0")), # if more than now, likely last century
+                         paste0("20", formatC(x[ypos], width = 2, flag = "0")))) # if less than now, likely this century
+      m <- formatC(x[mpos], width = 2, flag = "0")
+      if(m=="00") m <- "01"
+      d <- formatC(x[dpos], width = 2, flag = "0")
+      if(d=="00") d <- "01"
+      out <- paste(y,m,d,sep = "-")
+    } else {
+      out <- apply(x, 1, function(x){
+        y <- ifelse(nchar(x[ypos])>2,
+                    ifelse(x[ypos] > thresh & x[ypos]!="9999",
+                           paste0("19",x[ypos] %% 100),
+                           formatC(x[ypos], width = 4, flag = "0")), # if 4, keep or correct
+                    ifelse(as.numeric(x[ypos]) > (thresh %% 100),
+                           paste0("19", formatC(x[ypos], width = 2, flag = "0")), # if more than now, likely last century
+                           paste0("20", formatC(x[ypos], width = 2, flag = "0")))) # if less than now, likely this century
+        m <- formatC(as.numeric(x[mpos]), width = 2, flag = "0")
+        if(m=="00") m <- "01"
+        d <- formatC(as.numeric(x[dpos]), width = 2, flag = "0")
+        if(d=="00") d <- "01"
+        paste(y,m,d,sep = "-")
+      })
+    }
+    dates <- out
+  }
+  
   dates <- ifelse(stringr::str_detect(dates, "^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$"), as.character(as.Date(dates,"%d-%m-%Y")), dates) # Correct date order if need
   dates <- ifelse(stringr::str_detect(dates, "^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$"), as.character(as.Date(dates,"%d-%m-%Y")), dates) # Correct date order and size 
   dates <- ifelse(stringr::str_detect(dates, "^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$"), as.character(as.Date(dates,"%d-%m-%Y")), dates) # Correct date order and size
