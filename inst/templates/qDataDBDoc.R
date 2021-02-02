b539c8bfce1dd7c770e2c64d83636c901fa818ff
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-02-02 12:21:44 +0100

    Added database description template

---
+++ inst/templates/qDataDBDoc.R
@@ -0,0 +1,7 @@
+#' Write the database citation here.
+#' 
+#' @format The {{{database}}} database is a list that contains the following {{{nd}}} datasets: {{{strdsnames}}}
+{{{describe}}}
+{{{source}}}
+#' {{{database}}}
+"{{{dab}}}"
