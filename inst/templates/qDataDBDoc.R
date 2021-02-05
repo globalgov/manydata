2ca5a0beb89bf797d198304069e9d492c8a6ec66
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-02-05 10:01:55 +0100

    Closed #76, #78 and #92 by expanding report_data() function and updating database documentation

--- inst/templates/qDataDBDoc.R
+++ inst/templates/qDataDBDoc.R
@@ -2,7 +2,7 @@
 #' 
 #' Write the database citation here.
 #' 
-#' @format The {{{database}}} database is a list that contains the following {{{nd}}} datasets: {{{strdsnames}}}
+#' @format The {{{database}}} database is a list that contains the following {{{nd}}} datasets: {{{strdsnames}}}. For more info, please use the report_data() function. 
 {{{describe}}}
 {{{source}}}
 "{{{database}}}"
