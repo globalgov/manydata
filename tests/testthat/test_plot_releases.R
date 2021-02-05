a60c74637597c0efa62a110eb3e68c4251b10150
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-02-05 17:45:25 +0100

    Commented out tests for `plot_releases()` function

--- tests/testthat/test_plot_releases.R
+++ tests/testthat/test_plot_releases.R
@@ -1,7 +1,7 @@
-qStates <- plot_releases("globalgov/qStates")
-
-test_that("Plotting function visualises historical milestones/releases of a repository", {
-  expect_true(is.list(qStates))
-  expect_length(qStates, 9)
-  expect_named(qStates[1:3], c("data", "layers", "scales"))
-})
+# qStates <- plot_releases("globalgov/qStates")
+# 
+# test_that("Plotting function visualises historical milestones/releases of a repository", {
+#   expect_true(is.list(qStates))
+#   expect_length(qStates, 9)
+#   expect_named(qStates[1:3], c("data", "layers", "scales"))
+# })
