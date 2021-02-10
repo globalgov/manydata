a6eaf279dbce15d39d047957b1bb43bd9d4e14c2
EstherPeev <esther.peev@graduateinstitute.ch>
2021-02-10 14:08:22 +0100

    Modified minor coding parts in setup_package() function
    Family and given were not properly defined for the second author

--- R/package_setup.R
+++ R/package_setup.R
@@ -60,10 +60,10 @@ setup_package <- function(packageName = NULL,
   # Step one: ensure/create package/project structure
   # Add DESCRIPTION
   if(stringr::str_detect(packageAuthor, "\\,\\s") == "TRUE") {
-     given <- stringr::str_split(packageAuthor, "\\s|\\,")[[1]][1]
-     family <- stringr::str_split(packageAuthor, "\\s|\\,")[[1]][2]
-     given2 <- stringr::str_split(packageAuthor, "\\s|\\,")[[1]][3]
-     family2 <- stringr::str_split(packageAuthor, "\\s|\\,")[[1]][4]
+     given <- stringr::str_split(packageAuthor, "\\s|\\,\\s")[[1]][1]
+     family <- stringr::str_split(packageAuthor, "\\s|\\,\\s")[[1]][2]
+     given2 <- stringr::str_split(packageAuthor, "\\s|\\,\\s")[[1]][3]
+     family2 <- stringr::str_split(packageAuthor, "\\s|\\,\\s")[[1]][4]
      qtemplate("qPackage-DESC2.dcf",
               "DESCRIPTION",
               data = list(package = packageName,
