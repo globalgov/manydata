f9d38843d7fa1de85241c67115635813f1b696ee
EstherPeev <esther.peev@graduateinstitute.ch>
2021-02-10 15:59:59 +0100

    Deleted whitespace and added line break

--- inst/templates/qDataDBDoc.R
+++ inst/templates/qDataDBDoc.R
@@ -1,6 +1,7 @@
 #' {{{database}}} database documentation
-#' 
-#' @format The {{{database}}} database is a list that contains the following {{{nd}}} datasets: {{{strdsnames}}}. For more information and references to each of the datasets used, please use the report_data() function. 
+#'
+#' @format The {{{database}}} database is a list that contains the following {{{nd}}} datasets: {{{strdsnames}}}.
+#' For more information and references to each of the datasets used, please use the report_data() function.
 {{{describe}}}
 {{{sourceelem}}}
 "{{{database}}}"
