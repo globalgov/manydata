3ead809cfa6d40ab09aa09356a17993488582844
EstherPeev <esther.peev@graduateinstitute.ch>
2021-02-11 15:25:16 +0100

    Fixed #116 by adding more tests for dates variables and NA

--- inst/templates/test_agreements.R
+++ inst/templates/test_agreements.R
@@ -5,6 +5,10 @@ test_that("missing observations are reported correctly", {
   expect_false(any(grepl("\\?", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("^n/a$", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("^N/A$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("^\\s$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("^\\.$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("N\\.A\\.$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("n\\.a\\.$", {{{dab}}}[["{{{dat}}}"]])))
 })
 
 # Uniformity tests (agreements have a source ID, a string title, a signature and
@@ -14,9 +18,47 @@ test_that("datasets have the correct variables", {
   expect_col_exists({{{dab}}}[["{{{dat}}}"]], vars(Beg))
 })
 
-# Dates are standardized
+# Dates are standardized for mandatory column
 test_that("dates are standardised", {
   expect_col_is_date({{{dab}}}[["{{{dat}}}"]], vars(Beg))
   expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Beg)))
-  expect_false(any(grepl("\\?", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
 })
+
+# Dates are standardized for optional columns
+test_that("Columns with dates are standardized", {
+  if(!is.null({{{dab}}}[["{{{dat}}}"]]$End)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$End)))
+  }
+  if (!is.null({{{dab}}}[["{{{dat}}}"]]$Force)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+  }
+  if (!is.null({{{dab}}}[["{{{dat}}}"]]$Rat)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+  }
+})
+
+
--- inst/templates/test_general.R
+++ inst/templates/test_general.R
@@ -5,12 +5,58 @@ test_that("missing observations are reported correctly", {
   expect_false(any(grepl("\\?", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("^n/a$", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("^N/A$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("^\\s$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("^\\.$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("N\\.A\\.$", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("n\\.a\\.$", {{{dab}}}[["{{{dat}}}"]])))
 })
 
 # Labels are standardized
 test_that("labels are standardised", {
-  # expect_false(any(grepl("U.S.", {{{dab}}}[["{{{dat}}}"]])))
-  # expect_false(any(grepl("U.K.", {{{dab}}}[["{{{dat}}}"]])))
+  if(!is.null({{{dab}}}[["{{{dat}}}"]]$Label)) {
+  expect_false(any(grepl("U.S.", {{{dab}}}[["{{{dat}}}"]])))
+  expect_false(any(grepl("U.K.", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("!", {{{dab}}}[["{{{dat}}}"]])))
   expect_false(any(grepl("NANA.", {{{dab}}}[["{{{dat}}}"]])))
+  }
+})
+
+# Dates are standardized
+test_that("Columns with dates are standardized", {
+  if(!is.null({{{dab}}}[["{{{dat}}}"]]$Beg)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Beg)))
+  } 
+  if(!is.null({{{dab}}}[["{{{dat}}}"]]$End)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$End)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$End)))
+  }
+  if (!is.null({{{dab}}}[["{{{dat}}}"]]$Force)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Force)))
+  }
+  if (!is.null({{{dab}}}[["{{{dat}}}"]]$Rat)) {
+    expect_false(any(grepl("/", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{1}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{1}-[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:digit:]{2}-[:digit:]{4}$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+    expect_false(any(grepl("^[:alpha:]$", {{{dab}}}[["{{{dat}}}"]]$Rat)))
+  }
 })
