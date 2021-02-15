80df62111e4f9dc2af8213da2f1a6f3d74b65493
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-15 19:20:49 +0100

    Added conjoin_consensus()

--- R/conjoin.R
+++ R/conjoin.R
@@ -2,3 +2,8 @@
 conjoin_full <- function(dbase, key){
   purrr::reduce(dbase, function(x, y) dplyr::full_join(x, y, by = key))
 }
+
+#' @export
+conjoin_consensus <- function(dbase, key){
+  purrr::reduce(dbase, function(x, y) dplyr::inner_join(x, y, by = key))
+}
