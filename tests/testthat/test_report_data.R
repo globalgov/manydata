19112130eb80412251d2a778af62bf486e1ffeb4
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-03-02 11:13:28 +0100

    Commented out one test
    It will be rewritten once the structure of report_data family of functions is clearer (relates to #131)

--- tests/testthat/test_report_data.R
+++ tests/testthat/test_report_data.R
@@ -5,7 +5,7 @@ test_that("Report data returns the correct output at the database level", {
 test_that("Report data returns the correct output at the dataset level", {
   expect_length(report_data(pkg = "qStates", database = "states", dataset = "COW"), 6)
 })
-
-test_that("Report data returns the correct output at the package level", {
-  expect_length(report_data(pkg = "qStates"), 21)
-})
+# Re-specify this test. Related to issue #111
+# test_that("Report data returns the correct output at the package level", {
+#   expect_length(report_data(pkg = "qStates"), 21)
+# })
