0d7f2fac31194f1f3dbd7a6e431ce6eea972c2d6
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-03-04 12:49:21 +0100

    Updates to "code_agreements()" function and arguments

--- R/code_agreements.R
+++ R/code_agreements.R
@@ -1,22 +1,122 @@
+#' @param dataset Name of the dataset
+#' @param title Title collumn variable
+#' @param parties Parties to an agreement
+#' @param topic Abbreviated topic of an agreement 
+#' @param type Agreement type
+#' @param uID Unique agreement ID
+#' @param lineage Whether agreement is part of a lineage of agreements
 #' @export
-code_agreements <- function(title, beg){
-  parties <- qStates::code_states(title)
+code_agreements <- function(dataset, title, beg, parties, topic, type, uID, lineage) {
+  
+  if(missing(dataset)){
+    stop("Please declare the dataset.")
+  }
+  if(missing(title)){
+    stop("Please declare a title column.")
+  }
+  if(missing(beg)){
+    stop("Please declare a beg column.")
+  }
+  
+  # Step one: create a new qID column
+  qID <- purrr::map(title, as.character)
+  # Eventually this will connect to a centralized GitHub repo or SQL file for 
+  # smarter, interactive and more consistent coding of agreement titles. 
+  
+  # Step two: code parties if present
+  parties <- qStates::code_states(qID)
   parties <- stringr::str_replace_all(parties, "_", "-")
   parties[!grepl("-", parties)] <- NA
-  # Added topic abbreviations but other categories still need to be added
+  
+  # Step three: code agreement topic
+  # Added some topic abbreviations but other categories still need to be added
   topic <- case_when(
-    grepl("Waste|waste|disposal|pollut|toxic|hazard", title, ignore.case = T) ~ "WAS",
-    grepl("species|habitat|ecosystems|biological diversity|genetic resources|biosphere", title, ignore.case = T) ~ "SPE",
-    grepl("air|atmos|climate|outer space|ozone|emissions", title, ignore.case = T) ~ "AIR",
-    grepl("water|freshwater|river|rhine|hydro|basin|drought", title, ignore.case = T) ~ "WAT",
-    grepl("soil|wetland|desert|erosion", title, ignore.case = T) ~ "SOI",
-    grepl("nature|environment|biodiversity|flora|plant|fruit|vegetable|seed|forest|tree", title, ignore.case = T) ~ "BIO",
+    grepl("Waste|waste|disposal|pollut|toxic|hazard", qID, ignore.case = T) ~ "WAS",
+    grepl("species|habitat|ecosystems|biological diversity|genetic resources|biosphere", qID, ignore.case = T) ~ "SPE",
+    grepl("air|atmos|climate|outer space|ozone|emissions", qID, ignore.case = T) ~ "AIR",
+    grepl("water|freshwater|river|rhine|hydro|basin|drought", qID, ignore.case = T) ~ "WAT",
+    grepl("soil|wetland|desert|erosion", qID, ignore.case = T) ~ "SOI",
+    grepl("nature|environment|biodiversity|flora|plant|fruit|vegetable|seed|forest|tree", qID, ignore.case = T) ~ "BIO",
   )
-  out <- paste(parties, beg, topic, sep = "_")
+  
+  # Step four: code agreement type 
+  type <- case_when(
+    # E stands for amendment
+    grepl("amend|modify|extend|proces-verbal", qID, ignore.case = T) ~ "E",
+    # P stands for protocols
+    grepl("protocol|additional|subsidiary|supplementary|complÃ©mentaire|complementar|complementario", qID, ignore.case = T) ~ "P",
+    # A stands for agreements
+    grepl("agreement|arrangement|accord|acuerdo|bilateral co|technical co|treat|trait|tratado|convention|convencion|convenio|constitution|charte|instrument|statute|estatuto|provisional understanding|provisions relating|übereinkunft", v, ignore.case = T) ~ "A",
+    grepl("Act|Declaration|Covenant|Scheme|Government Of|Law", qID, ignore.case = T) ~ "A",
+  )
+  
+  #step five: give the observation a unique ID
+  uID <- function(title) {
+    # should detect similarities based and give same unique IDs to similar obs
+#     
+#     uID <- stringr::str_remove_all(beg, "-")
+#     
+#     for (i in maybe(tittle)) {
+#       if (i == TRUE){
+#         menu(c("Yes", "No"), title = "Are these the same treaty/agreement or amendments or protocols to the same treaty/agreement?")  
+#       }
+#     
+#     case_when(
+#       # same agreements same uIDs
+#     ) 
+#     
+#     case_when(
+#       # user decided on ID
+#     )
+# }
+#     
+#   # adapted from: https://stackoverflow.com/questions/12999772/create-a-unique-id-by-fuzzy-matching-of-names-via-agrep-using-r
+#     
+# same_agreements <- function(x) {
+#   x <- as.factor(x)
+#   matches <- lapply(levels(x), agrep, x=levels(x),fixed=TRUE, value=FALSE)
+#   levels(x) <- levels(x)[unlist(lapply(matches, function(x) x[1:10]))]
+#   as.character(x)
+# }
+#     
+# maybe <- function(x) {
+#   x <- as.factor(x)
+#   matches <- lapply(levels(x), agrep, x=levels(x),fixed=TRUE, value=FALSE)
+#   levels(x) <- levels(x)[unlist(lapply(matches, function(x) x[10:20]))]
+#   as.character(x)
+# }
+}
+   
+  #Step six: identify if agreement is part of a lineage
+  lineage <- case_when(
+    # should order observations with the same unique IDs as part of the same lineage
+    # maybe this can be done according to date in beg
+  )
+  
+  # Step seven: add items together correctly   
+  if(is.na == parties) {
+    if(is.null(lineage)) {
+      out <- paste0(topic, "_", uID, "-", type)  
+    } else {
+    out <- paste(topic, "_", uID, "-", type, lineage)
+    }
+  } else {
+    if(is.null(lineage)) {
+      out <- paste0(parties, "_", topic, "_", uID, "-", type)  
+    } else {
+    out <- paste0(parties, "_", topic, "_", uID, "-", type, lineage)
+    }
+  }
+  
   out <- stringr::str_replace_all(out, "NA_", NA_character_)
   cat(sum(is.na(out)), "entries were not matched at all.\n")
   # cat(sum(stringr::str_detect(out, "^[0-9]")), " entries were only coded by date.\n")
   cat("There were", sum(duplicated(out, incomparables = NA)), "duplicated IDs.\n")
-  out
-}
+  qID <- out
+  
+  # Step eight: add new qID column to data
+  cbind(dataset, qID)
+  
+  qID 
 
+}
