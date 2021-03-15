5833ec98169b268aeb39e30ba2a729d83f849288
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-03-15 19:01:43 +0100

    Minor updates to resolve script

--- R/resolve.R
+++ R/resolve.R
@@ -6,6 +6,59 @@
 #' @param key An ID column to collapse by
 NULL
 
+#' @rdname resolve
+#' @importFrom purrr map
+#' @export
+resolve_min <- function(var){
+  unlist(purrr::map(var, function(x) min(x)))
+}
+
+#' @rdname resolve
+#' @importFrom purrr map
+#' @export
+resolve_max <- function(var){
+  unlist(purrr::map(var, function(x) max(x)))
+}
+
+#' @rdname resolve
+#' @importFrom purrr map
+#' @export
+resolve_mean <- function(var){
+  if(is.character(var[[1]])){
+    resolve_median(var)
+  } else {
+    unlist(purrr::map(var, function(x) mean(x)))
+  }
+}
+
+#' @rdname resolve
+#' @importFrom purrr map
+#' @export
+resolve_median <- function(var){
+  unlist(purrr::map(var, function(x){
+    if(length(x)==1){
+      x
+    } else {
+      if(is.numeric(var[[1]])){
+        median(x)
+      } else {
+        x[round(length(x)/2)]
+      }
+    }
+  }))
+}
+
+#' @rdname resolve
+#' @importFrom purrr map
+#' @export
+resolve_mode <- function(var){
+  Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }
+  unlist(purrr::map(var, function(x) Mode(x)))
+}
+
 #' Resolve Date Ranges
 #'
 #' @details This function resolves ranged dates created with `standardise_dates()`
@@ -155,59 +208,3 @@ resolve_dates <- function(dates, type = c("mean", "min", "max")) {
   dates <- lubridate::as_date(dates)
   dates
 }
-
-#' @rdname resolve
-#' @importFrom purrr map
-#' @export
-resolve_min <- function(var){
-  unlist(purrr::map(var, function(x) min(x)))
-}
-
-#' @rdname resolve
-#' @importFrom purrr map
-#' @export
-resolve_max <- function(var){
-  unlist(purrr::map(var, function(x) max(x)))
-}
-
-#' @rdname resolve
-#' @importFrom purrr map
-#' @export
-resolve_mean <- function(var){
-  if(is.character(var[[1]])){
-    resolve_median(var)
-  } else {
-    unlist(purrr::map(var, function(x) mean(x)))
-  }
-}
-
-#' @rdname resolve
-#' @importFrom purrr map
-#' @export
-resolve_median <- function(var){
-  unlist(purrr::map(var, function(x){
-    if(length(x)==1){
-      x
-    } else {
-      if(is.numeric(var[[1]])){
-        median(x)
-      } else {
-        x[round(length(x)/2)]
-      }
-    }
-  }))
-}
-
-#' @rdname resolve
-#' @importFrom purrr map
-#' @export
-resolve_mode <- function(var){
-  Mode <- function(x) {
-    ux <- unique(x)
-    ux[which.max(tabulate(match(x, ux)))]
-  }
-  unlist(purrr::map(var, function(x) Mode(x)))
-}
-
-
-
