033948f6b6cf52254913473ec319d1eda54499e2
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-03-22 20:36:18 +0100

    Updated tests for `setup_package()` and added a test for `add_author()`

--- tests/testthat/test_setup_package.R
+++ tests/testthat/test_setup_package.R
@@ -2,11 +2,19 @@ test_that("package name is required", {
   expect_error(setup_package(), "Please declare a package name")
 })
 test_that("author names must be declared", {
-  expect_error(setup_package("qtest"), "Please declare at least one author")
+  expect_error(setup_package("qtest"), "Please declare one author")
 })
 
 test_that("package name must start with the letter q", {
   expect_error(setup_package("test"), "Package name must start with a 'q'")
+})
+
+test_that("number of authors taken is correct", {
+  expect_error(setup_package("qTest", name = c("Hollway, James", "Sposito, Henrique")), "Please specify author. Add the rest by using our add_author() function.")
+})
+  
+test_that("additional authors are added correctly", {
+  expect_error(add_author(), "Either a correct ORCID number or name in the format 'Surname, Given Names' must be provided.")
 })
 
 # test_that("setup_package has expected file structure", {
