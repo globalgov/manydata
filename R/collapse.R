14606cf369de98319c3f9d94246c3c1107d5a246
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-03-26 15:09:09 +0100

    Added examples to collapse function documentation and made other minor documentation updates

--- R/collapse.R
+++ R/collapse.R
@@ -1,5 +1,8 @@
 #' Collapse a database into a single data frame/tibble
 #' 
+#' @description The collapse family of functions collapses
+#' lists of datasets that form a database in qPackages
+#' according to user preferences.
 #' @name collapse
 #' @param dbase A qPackage database object
 #' @param dset A dataset label from within that database
@@ -11,6 +14,7 @@ NULL
 
 #' @rdname collapse
 #' @importFrom purrr pluck
+#' @example collapse_select(states, "COW")
 #' @export
 collapse_select <- function(dbase, dset){
   purrr::pluck(dbase, dset)
@@ -19,6 +23,7 @@ collapse_select <- function(dbase, dset){
 #' @rdname collapse
 #' @importFrom purrr reduce
 #' @importFrom dplyr full_join
+#' @example collapse_full(states, "ID")
 #' @export
 collapse_full <- function(dbase, key, resolve = NULL){
   if(is.null(resolve)) {
@@ -38,6 +43,7 @@ collapse_full <- function(dbase, key, resolve = NULL){
 #' @rdname collapse
 #' @importFrom purrr reduce
 #' @importFrom dplyr inner_join
+#' @example collapse_consensus(states, "ID")
 #' @export
 collapse_consensus <- function(dbase, key, resolve = NULL){
   if(is.null(resolve)) {
