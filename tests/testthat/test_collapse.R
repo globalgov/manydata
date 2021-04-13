49e09cd4a5d6507587978c937dfa8314f2fe2cc8
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-04-13 19:14:50 +0200

    Fixed tests of the collapse function
    Still need to add some for the newly created collapse_right and collapse_left functions though

--- tests/testthat/test_collapse.R
+++ tests/testthat/test_collapse.R
@@ -1,19 +1,37 @@
 test_that("collapse works", {
-  data1 <- data.frame(ID = c("NZL", "BRA", "CHF"), date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31"))
-  data2 <- data.frame(ID = c("NZL", "BRA"), date = c("1990-01-01","1990-01-02"))
-  data3 <- data.frame(ID = c("NZL", "BRA", "CHF", "OTH"), date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA))
+  data1 <- dplyr::tibble(ID = c("NZL", "BRA", "CHF"), 
+                         date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31"))
+  data2 <- dplyr::tibble(ID = c("NZL", "BRA"), 
+                         date = c("1990-01-02","1990-01-03"))
+  data3 <- dplyr::tibble(ID = c("NZL", "BRA", "CHF", "OTH"), 
+                         date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA))
   test <- tibble::lst(a = data1, b = data2, c = data3)
+  # Passes :)
   expect_equal(collapse_select(test, "a"), data1)
-  test1 <- data.frame(ID = c("NZL", "BRA", "CHF", "OTH"), date.x = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA),
-                      date.y = c("1990-01-01","1990-01-02", NA, NA), date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA))
+  test1 <- dplyr::tibble(ID = c("NZL", "BRA", "CHF", "OTH"), 
+                         date.x = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA),
+                         date.y = c("1990-01-02","1990-01-03", NA, NA),
+                         date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA))
   expect_equal(collapse_full(test, "ID"), test1)
-  test2 <- data.frame(ID = c("NZL", "BRA"), date.x = c("1990-01-01","1990-01-02"),
-                      date.y = c("1990-01-01","1990-01-02"), date = c("1990-01-01","1990-01-02"))
+  # Passes :)
+  test2 <- dplyr::tibble(ID = c("NZL", "BRA"), 
+                         date.x = c("1990-01-01","1990-01-02"),
+                         date.y = c("1990-01-02","1990-01-03"),
+                         date = c("1990-01-01","1990-01-02"))
   expect_equal(collapse_consensus(test, "ID"), test2)
-  test3 <- data.frame(ID.x = c("NZL", "BRA"), date = c("1990-01-01","1990-01-02"),
-                      ID.y = c("NZL", "BRA"), ID = c("NZL", "BRA"))
+  # Passes :)
+  test3 <- dplyr::tibble(ID.x = c("BRA"), date = c("1990-01-02"),
+                      ID.y = c("NZL"), ID = c("BRA"))
   expect_equal(collapse_consensus(test, "date"), test3)
-  test4 <- data.frame(ID.x = c("NZL", "BRA", "CHF", NA), date = c("1990-01-01","1990-01-02","1990-01-01", NA),
-                             ID.y = c("NZL", "BRA", NA, NA), ID = c("NZL", "BRA", "CHF", "OTH"))
+  # We join on date to test the resolve implementation for uncertain keys. 
+  # This will not often be used in practice though.
+  test4 <- dplyr::tibble(ID.x = c("NZL", "NZL", "BRA", "CHF", "CHF", NA, NA),
+                          date = c("1990-01-01","1990-01-01","1990-01-02", "1990-01-01","1990-01-01","1990-01-03",NA),
+                          ID.y = c(NA, NA, "NZL", NA, NA, "BRA", NA), 
+                         ID = c("NZL", "CHF", "BRA", "NZL", "CHF", NA, "OTH"))
   expect_equal(collapse_full(test, "date", resolve = "min"), test4)
+  # Does it make sense to join on date?
+  # Passes :)
 })
+
+# For now the resolve option only resolves on the key variable by design. We might want to expand this.
