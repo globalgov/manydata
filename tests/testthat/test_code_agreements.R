78e7b60faa84cfa691ca9df26f3bd21a07905ae4
EstherPeev <esther.peev@graduateinstitute.ch>
2021-04-21 18:21:35 +0200

    Added one test for code_agreements representing the case when dates are ranges

--- tests/testthat/test_code_agreements.R
+++ tests/testthat/test_code_agreements.R
@@ -18,3 +18,19 @@ test_that("Code_agreements helper functions work properly", {
   expect_equal(code_linkage(data$title, data$date), c("", "", "A19710202", "A19710202", "A19761203", "A19761203"))
 })
     
+# Add one test for dataset that have range as dates
+data2 <- data.frame(title = c("Agreement Between Cape Verde And Portugal On Fisheries Development", 
+                             "Traité De Délimitation Maritime, Signé À Paris Le 30 Janvier 1981",
+                             "Convention On Wetlands Of International Importance Especially As Waterfowl Habitat",
+                             "Protocol To Amend The Convention On Wetlands Of International Importance Especially As Waterfowl Habitat", 
+                             "Convention On The Protection Of The Rhine Against Pollution By Chlorides",
+                             "Amendment 1 to the Convention On The Protection Of The Rhine Against Pollution By Chlorides"),
+                   date = c("1980", "1981", "1971", "1982", "1976", "1983"))
+test_that("code_dates() helper function treats date range correctly", {
+  # Add title to the code_dates function arguments 
+  expect_equal(code_dates(data2$title, data2$date), c("1980AT", "1981TR", "1971CT", "1982PS", "1976CS", "1983AS"))
+})
+
+
+
+
