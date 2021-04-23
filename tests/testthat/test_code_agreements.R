980b7a849c23e2579a8ba11f398938ffd588c22b
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-04-23 18:57:54 +0200

    Minor updates to code_agreements()

--- tests/testthat/test_code_agreements.R
+++ tests/testthat/test_code_agreements.R
@@ -1,13 +1,14 @@
 data <- data.frame(title = c("Agreement Between Cape Verde And Portugal On Fisheries Development", 
+                             "Protocol to the Agreement Between Cape Verde And Portugal On Fisheries Development",
                               "Traité De Délimitation Maritime, Signé À Paris Le 30 Janvier 1981",
                               "Convention On Wetlands Of International Importance Especially As Waterfowl Habitat",
                               "Protocol To Amend The Convention On Wetlands Of International Importance Especially As Waterfowl Habitat", 
                               "Convention On The Protection Of The Rhine Against Pollution By Chlorides",
                               "Amendment 1 to the Convention On The Protection Of The Rhine Against Pollution By Chlorides"),
-                    date = c("1980-05-08", "1981-01-30", "1971-02-02", "1982-12-03", "1976-12-03", "1983-04-29"))
+                    date = c("1980-05-08", "1990-12-31", "1981-01-30", "1971-02-02", "1982-12-03", "1976-12-03", "1983-04-29"))
 
 test_that("Code_agreements() properly returns qIDs", {
-  expect_equal(code_agreements(data$title, data$date), c("19800508A_CPV-PRT", "19810130A",   "RAMSA19710202A", "19821203P_RAMSA19710202", "19761203A", "19830429E1_19761203A"))
+  expect_equal(code_agreements(data$title, data$date), c("19800508A_CPV-PRT", "19901231P_19800508_CPV-PRT", "19810130A",   "RAMSA19710202A", "19821203P_RAMSA19710202", "19761203A", "19830429E1_19761203A"))
 })
 
 test_that("Code_agreements helper functions work properly", {
