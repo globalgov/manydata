63804c2b9d0f618edf2e0aa3b9fafee74b10ae33
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-04-28 10:03:29 +0200

    Updated tests for code_agreements()

--- tests/testthat/test_code_agreements.R
+++ tests/testthat/test_code_agreements.R
@@ -8,15 +8,15 @@ data <- data.frame(title = c("Agreement Between Cape Verde And Portugal On Fishe
                     date = c("1980-05-08", "1990-12-31", "1981-01-30", "1971-02-02", "1982-12-03", "1976-12-03", "1983-04-29"))
 
 test_that("Code_agreements() properly returns qIDs", {
-  expect_equal(code_agreements(data$title, data$date), c("19800508A_CPV-PRT", "19901231P_19800508_CPV-PRT", "19810130A",   "RAMSA19710202A", "19821203P_RAMSA19710202", "19761203A", "19830429E1_19761203A"))
+  expect_equal(code_agreements(data$title, data$date), c("19800508_CPV-PRT", "19901231P_19800508_CPV-PRT", "19810130A",   "RAMSA19710202", "19821203P_RAMSA19710202", "19761203A", "19830429E1_19761203A"))
 })
 
 test_that("Code_agreements helper functions work properly", {
-  expect_equal(code_parties(data$title), c("CPV-PRT", NA, NA, NA, NA, NA))
-  expect_equal(code_type(data$title), c("A", "A", "A", "P", "A", "E1"))
-  expect_equal(code_dates(data$title, data$date), c("19800508", "19810130", "19710202", "19821203", "19761203", "19830429"))
-  expect_equal(code_known_agreements(data$title), c(NA, NA, "RAMSA", "RAMSA", NA, NA))
-  expect_equal(code_linkage(data$title, data$date), c("", "", "19710202A", "19710202A", "19761203A", "19761203A"))
+  expect_equal(code_parties(data$title), c("CPV-PRT", "CPV-PRT", NA, NA, NA, NA, NA))
+  expect_equal(code_type(data$title), c("A", "P", "A", "A", "P", "A", "E1"))
+  expect_equal(code_dates(data$title, data$date), c("19800508", "19901231", "19810130", "19710202", "19821203", "19761203", "19830429"))
+  expect_equal(code_known_agreements(data$title), c(NA, NA, NA, "RAMSA19710202", "RAMSA19710202", NA, NA))
+  expect_equal(code_linkage(data$title, data$date), c("19800508_CPV-PRT", "19800508_CPV-PRT", "", "19710202A", "19710202A", "19761203A", "19761203A"))
 })
     
 # Add one test for dataset that have range as dates
