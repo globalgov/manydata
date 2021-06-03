92c7a1f0a6cd3eccd1bacea6e4b7b315d410beb0
James Hollway <james.hollway@graduateinstitute.ch>
2021-06-03 20:53:59 +0200

    Added concentrate with any options

--- R/concentrate.R
+++ R/concentrate.R
@@ -1,5 +1,69 @@
+#' Concentrate database into a single dataset
+#' 
+#' This function collapses a set or database of (q)datasets
+#' into a single dataset with some combination of the
+#' rows, columns, and observations of the parent datasets.
+#' The function includes separate arguments for the rows and columns, 
+#' as well as for how to resolve conflicts in observations across datasets.
+#' This provides users with considerable flexibility in how they combine qData.
+#' For example, users may wish to stick to units that appear in every dataset
+#' but include variables coded in any dataset, or units that appear in any dataset
+#' but only those variables that appear in every dataset.
+#' Even then there may be conflicts, as the actual unit-variable observations
+#' may differ from dataset to dataset.
+#' Here we (will) offer a number of resolve methods that enable users to choose
+#' how conflicts between observations are resolved.
+#' @param .data A qPackage database object
+#' @param rows Which rows or units to retain.
+#' By default "any" (or all) units are retained,
+#' but another option is "every",
+#' which retains only those units that appear in all parent datasets.
+#' @param cols Which columns or variables to retain.
+#' By default "any" (or all) variables are retained,
+#' but another option is "every",
+#' which retains only those variables that appear in all parent datasets.
+#' @param resolve How conflicts between observations should be resolved.
+#' Currently only "coalesce" is offered, which takes the first non-NA value,
+#' but options should soon include 
+#' "max", "min", "mean", "mode" and "median", as well as "append".
+#' @param key An ID column to collapse by. By default "qID".
+#' @return A single tibble/data frame.
+#' @examples
+#' pluck(qStates::states, "COW")
+#' concentrate(qEnviron::agreements, "any", "any", "coalesce")
+#' concentrate(qEnviron::agreements, "every", "every", "coalesce")
+#' @export
+concentrate <- function(.data, 
+                        rows = c("any","every"), 
+                        cols = c("any","every"), 
+                        resolve = "coalesce",
+                        key = "qID"){
+  
+  # Step 1: Join datasets by ID
+  rows <- match.arg(rows)
+    out <- purrr::reduce(.data, dplyr::full_join, by = key)
+  # Step 2: Drop any unwanted variables
+  cols <- match.arg(cols)
+  all_variables <- unlist(purrr::map(.data, names))
+  
+  # Step 3: Resolve conflicts
+  resolve <- match.arg(resolve)
+  other_variables <- all_variables[!key == all_variables]
+  if(resolve == "coalesce"){
+    for(var in other_variables){
+      vars_to_combine <- startsWith(names(out), var)
+      new_var <- dplyr::coalesce(!!!out[vars_to_combine])
+      out <- out %>% dplyr::select(-dplyr::starts_with(var))
+      out[,var] <- new_var
+      out
+    }
+  }
+  
+  out
+}
 
 #' @importFrom purrr pluck
 #' @export
 purrr::pluck
+
 
--- tests/testthat/test_concentrate.R
+++ tests/testthat/test_concentrate.R
@@ -9,9 +9,18 @@ data3 <- dplyr::tibble(qID = c("NZL", "BRA", "CHF", "OTH"),
                        number = c(100, 1200, 12222, 21))
 test <- tibble::lst(a = data1, b = data2, c = data3)
 
+# expected output data
+data.13.any <- dplyr::tibble(qID = c("NZL","BRA","CHF","OTH"), 
+                                date = c("1990-01-01", "1990-01-02","1990-01-01:1990-01-31",NA),
+                                number = c(100,1000,10000,21))
+
 test_that("pluck works", {
   expect_equal(pluck(test, "a"), data1)
   expect_equal(pluck(test, "b"), data2)
   expect_equal(pluck(test, "c"), data3)
 })
 
+test_that("concentrate works", {
+  expect_equal(concentrate(test[c(1,3)], "any", "any"), data.13.any)
+  expect_equal(concentrate(test, "any"), data.13.any)
+})
