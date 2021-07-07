13fe4fd2ce1f6bde39cd9cbca90f2b9ec45d8a00
James Hollway <james.hollway@graduateinstitute.ch>
2021-07-07 21:32:45 +0200

    Added coercion back to Date, POSIXct, and POSIXlt by resolving messy dates

--- R/messydate.R
+++ R/messydate.R
@@ -255,6 +255,47 @@ mean.messydt <- function(..., na.rm = TRUE){
   y
 }
 
+#' @export
+as.Date.messydt <- function(x, 
+                            resolve = c("min","max","median","mean")){
+  
+  resolve <- match.arg(resolve)
+  
+  if(resolve == "min") x <- min(x)
+  if(resolve == "max") x <- max(x)
+  if(resolve == "mean") x <- mean(x)
+  if(resolve == "median") x <- median(x)
+  
+  as.Date(x)
+}
+
+#' @export
+as.POSIXct.messydt <- function(x, 
+                            resolve = c("min","max","median","mean")){
+  
+  resolve <- match.arg(resolve)
+  
+  if(resolve == "min") x <- min(x)
+  if(resolve == "max") x <- max(x)
+  if(resolve == "mean") x <- mean(x)
+  if(resolve == "median") x <- median(x)
+  
+  as.POSIXct(x)
+}
+
+#' @export
+as.POSIXlt.messydt <- function(x, 
+                               resolve = c("min","max","median","mean")){
+  
+  resolve <- match.arg(resolve)
+  
+  if(resolve == "min") x <- min(x)
+  if(resolve == "max") x <- max(x)
+  if(resolve == "mean") x <- mean(x)
+  if(resolve == "median") x <- median(x)
+  
+  as.POSIXlt(x)
+}
 
 #' @export
 print.messydt <- function(x){
