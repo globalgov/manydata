11aa613102dae0858e508de21212b67e0d79245b
James Hollway <james.hollway@graduateinstitute.ch>
2021-07-07 21:13:14 +0200

    Added validate_messydate() for validating messydate class objects (wip) 🚧🏗⚠️

--- R/messydate.R
+++ R/messydate.R
@@ -4,3 +4,26 @@ new_messydate <- function(x = character()){
   structure(x, class = c("messydt"))
 }
 
+#' @export
+validate_messydate <- function(x){
+  values <- unclass(x)
+  
+  if (any(grepl("[A-WYZa-z]", values))) {
+    stop(
+      "The only alpha character allowed in messy dates is 'X' for unspecified time components",
+      call. = FALSE
+    )
+  }
+
+    if (!all(grepl("[0-9]", values))) {
+    stop(
+      "Messy dates require at least one specified date component.",
+      call. = FALSE
+    )
+    }
+  
+  # can only consist of numbers and some special symbols: []{}..X%?~"
+  
+  x
+}
+
