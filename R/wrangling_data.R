cf9e6cd9950b2543245e8ba02f7a63514642d9c7
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-19 10:41:18 +0200

    Deleted resolve script and moved repaint() to wrangling_data script

--- R/wrangling_data.R
+++ R/wrangling_data.R
@@ -104,3 +104,30 @@ recollect <- function(x, collapse = "_") {
 coalesce_rows <- function(x) {
   x[which(!is.na(x))[1]]
 }
+
+#' Fills missing data by lookup
+#'
+#' Fills missing data where known by other observations with the same id/index
+#' @param df a dataframe
+#' @param id a string identifying a column in the dataframe for indexing
+#' @param var a string identifying a column or columns in the dataframe
+#' to be filled
+#' @return a dataframe
+#' @examples
+#' \dontrun{
+#' myData <- repaint(myData, id="StatID", var=c("Area","Region")
+#' }
+#' @export
+repaint <- function(df, id, var) {
+  for (co in var) {
+    for (ea in unique(df[, id])) {
+      if (any(!is.na(df[df[, id] == ea, co])) &
+          any(is.na(df[df[, id] == ea, co]))) {
+        df[df[,id] == ea &
+             is.na(df[, co]), co] <- df[df[, id] == ea &
+                                          !is.na(df[, co]), co][1]
+      }
+    }
+  }
+  df
+}
