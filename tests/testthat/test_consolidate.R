93212f3b506accc508d7258c50d521bcb9a008d4
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-20 11:00:56 +0200

    Updated and simplified tests for report_data() to fix for bug with package dependencies

--- tests/testthat/test_consolidate.R
+++ tests/testthat/test_consolidate.R
@@ -1,21 +1,21 @@
 # input data
-data1 <- dplyr::tibble(qID = c("NZL", "BRA", "CHF"), 
+data1 <- dplyr::tibble(qID = c("NZL", "BRA", "CHF"),
                        date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31"),
                        number = c(100, 1000, 10000))
-data2 <- dplyr::tibble(qID = c("NZL", "BRA"), 
+data2 <- dplyr::tibble(qID = c("NZL", "BRA"),
                        date = c("1990-01-01","1990-01-03"))
-data3 <- dplyr::tibble(qID = c("NZL", "BRA", "CHF", "OTH"), 
+data3 <- dplyr::tibble(qID = c("NZL", "BRA", "CHF", "OTH"),
                        date = c("1990-01-01","1990-01-02","1990-01-01:1990-01-31", NA),
                        number = c(100, 1200, 12222, 21))
 test <- tibble::lst(a = data1, b = data2, c = data3)
 
 # expected output data
-data.con.con <- dplyr::tibble(qID = c("NZL", "BRA"), 
+data.con.con <- dplyr::tibble(qID = c("NZL", "BRA"),
                                 date = c("1990-01-01","1990-01-02"))
-data.con.any <- dplyr::tibble(qID = c("NZL","BRA"), 
+data.con.any <- dplyr::tibble(qID = c("NZL","BRA"),
                               date = c("1990-01-01","1990-01-02"),
                               number = c(100,1000))
-data.13.any <- dplyr::tibble(qID = c("NZL","BRA","CHF","OTH"), 
+data.13.any <- dplyr::tibble(qID = c("NZL","BRA","CHF","OTH"),
                                 date = c("1990-01-01", "1990-01-02","1990-01-01:1990-01-31",NA),
                                 number = c(100,1000,10000,21))
 
--- tests/testthat/test_report_data.R
+++ tests/testthat/test_report_data.R
@@ -1,35 +1,19 @@
-# Set up "qStates" for testing
-get_packages("qStates")
-pkg <- "qStates"
-database <- "states"
-dataset <- "COW"
-
-test_that("data_source() returns the correct output format at the package
-          level", {
-    expect_type(data_source(pkg), "list")
-          })
-
-test_that("data_source() returns the correct output format at the database
-          level", {
+test_that("data_source() returns the correct output format", {
+  get_packages("qStates")
+  pkg <- "qStates"
+  database <- "states"
+  dataset <- "COW"
+  expect_type(data_source(pkg), "list")
   expect_type(data_source(pkg, database), "list")
-          })
-
-test_that("data_source() returns the correct output format at the dataset
-          level", {
   expect_type(data_source(pkg, database, dataset), "list")
-          })
+})
 
-test_that("data_contrast() returns the correct output format at the package
-          level", {
+test_that("data_contrast() returns the correct output format", {
+  get_packages("qStates")
+  pkg <- "qStates"
+  database <- "states"
+  dataset <- "COW"
   expect_type(data_contrast(pkg), "list")
-})
-
-test_that("data_contrast() returns the correct output format at the database
-          level", {
   expect_type(data_contrast(pkg, database), "list")
-})
-
-test_that("data_contrast() returns the correct output format at the dataset
-          level", {
   expect_type(data_contrast(pkg, database, dataset), "list")
 })
