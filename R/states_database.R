5170e1e3dae599bc8e267d2a141ea0926032b020
EstherPeev <esther.peev@graduateinstitute.ch>
2021-07-20 15:31:29 +0200

    Added documentation for databases

---
+++ R/states_database.R
@@ -0,0 +1,10 @@
+#' states database documentation
+#'
+#' @format The states database is a list that contains the
+#' following 3 datasets: GW, ISD, COW.
+#'\describe{
+#' \item{GW: }{A dataset with 150 observations and the following 5 variables: COW_Nr, ID, Beg, End, Label.}
+#' \item{ISD: }{A dataset with 150 observations and the following 7 variables: COW_Nr, ID, Beg, End, Label, Micro, New.State.}
+#' \item{COW: }{A dataset with 150 observations and the following 5 variables: COW_Nr, ID, Beg, End, Label.}
+#' }
+"states"
