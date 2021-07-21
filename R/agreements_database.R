4e0f3ee4102719404911de5f4c6e38911b74ac55
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-21 09:55:59 +0200

    Documentation upadtes to agreements short version data added to qData

--- R/agreements_database.R
+++ R/agreements_database.R
@@ -1,7 +1,11 @@
-#' agreements database documentation
+#' Agreements Database Documentation
 #'
-#' @format The agreements database is a list that contains the
-#' following 3 datasets: IEADB, GNEVAR, ECOLEX.
+#' @format The agreements database is a list that contains a shorter version
+#' of the following 3 datasets: IEADB, GNEVAR, ECOLEX.
+#' @details These datasets are to be used for testing and
+#' running examples and vignettes in qData.
+#' For the complete database and more datasets on environmental agreements
+#' please refer to the `{qEnviron}` package.
 #'\describe{
 #' \item{IEADB: }{A dataset with 200 observations and the following 8 variables: IEADB_ID, Title, Beg, L, D, Signature, Force, qID.}
 #' \item{GNEVAR: }{A dataset with 200 observations and the following 10 variables: GNEVAR_ID, Title, Beg, End, L, J, D, Signature, Force, qID.}
