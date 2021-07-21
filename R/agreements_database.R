ff4a84888021ff61a39e21c2347d0bab49abccc3
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-21 13:42:14 +0200

    Minor fixes to code issues pointed by lintr

--- R/agreements_database.R
+++ R/agreements_database.R
@@ -7,8 +7,11 @@
 #' For the complete database and more datasets on environmental agreements
 #' please refer to the `{qEnviron}` package.
 #'\describe{
-#' \item{IEADB: }{A dataset with 200 observations and the following 8 variables: IEADB_ID, Title, Beg, L, D, Signature, Force, qID.}
-#' \item{GNEVAR: }{A dataset with 200 observations and the following 10 variables: GNEVAR_ID, Title, Beg, End, L, J, D, Signature, Force, qID.}
-#' \item{ECOLEX: }{A dataset with 200 observations and the following 8 variables: ECOLEX_ID, Title, Beg, L, J, Signature, Force, qID.}
+#' \item{IEADB: }{A dataset with 200 observations and the following 8 variables:
+#' IEADB_ID, Title, Beg, L, D, Signature, Force, qID.}
+#' \item{GNEVAR: }{A dataset with 200 observations and the following 10 variables:
+#' GNEVAR_ID, Title, Beg, End, L, J, D, Signature, Force, qID.}
+#' \item{ECOLEX: }{A dataset with 200 observations and the following 8 variables:
+#' ECOLEX_ID, Title, Beg, L, J, Signature, Force, qID.}
 #' }
 "agreements"
--- R/states_database.R
+++ R/states_database.R
@@ -5,10 +5,13 @@
 #' @details These datasets are to be used for testing and
 #' running examples and vignettes in qData.
 #' For the complete database and more datasets on states,
-#' please refer to the `{qStates}` package. 
+#' please refer to the `{qStates}` package.
 #'\describe{
-#' \item{GW: }{A dataset with 150 observations and the following 5 variables: COW_Nr, ID, Beg, End, Label.}
-#' \item{ISD: }{A dataset with 150 observations and the following 7 variables: COW_Nr, ID, Beg, End, Label, Micro, New.State.}
-#' \item{COW: }{A dataset with 150 observations and the following 5 variables: COW_Nr, ID, Beg, End, Label.}
+#' \item{GW: }{A dataset with 150 observations and the following 5 variables:
+#' COW_Nr, ID, Beg, End, Label.}
+#' \item{ISD: }{A dataset with 150 observations and the following 7 variables:
+#' COW_Nr, ID, Beg, End, Label, Micro, New.State.}
+#' \item{COW: }{A dataset with 150 observations and the following 5 variables:
+#' COW_Nr, ID, Beg, End, Label.}
 #' }
 "states"
