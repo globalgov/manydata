583e7c22bba393387565d3654b41c8457e02b299
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-21 09:59:52 +0200

    Updated tests for report family of functions

--- tests/testthat/test_report_data.R
+++ tests/testthat/test_report_data.R
@@ -1,19 +1,11 @@
 test_that("data_source() returns the correct output format", {
-  get_packages("qStates")
-  pkg <- "qStates"
-  database <- "states"
-  dataset <- "COW"
-  expect_type(data_source(pkg), "list")
-  expect_type(data_source(pkg, database), "list")
-  expect_type(data_source(pkg, database, dataset), "list")
+  expect_type(data_source("qData"), "list")
+  expect_type(data_source("qData", "states"), "list")
+  expect_type(data_source("qData", "states", "COW"), "list")
 })
 
 test_that("data_contrast() returns the correct output format", {
-  get_packages("qStates")
-  pkg <- "qStates"
-  database <- "states"
-  dataset <- "COW"
-  expect_type(data_contrast(pkg), "list")
-  expect_type(data_contrast(pkg, database), "list")
-  expect_type(data_contrast(pkg, database, dataset), "list")
+  expect_type(data_contrast("qData", "states"), "list")
+  expect_type(data_contrast("qData", "states", "COW"), "list")
+  expect_invisible(contrast <- data_contrast("qData", "states", "COW"))
 })
