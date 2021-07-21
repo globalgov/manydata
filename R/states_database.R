26277247698ff95a9848ab0618421756af7d9c9b
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-21 09:56:44 +0200

    Minor documentation updates to states data added for running testing and examples in qData

--- R/states_database.R
+++ R/states_database.R
@@ -1,7 +1,11 @@
-#' states database documentation
+#' states Database Documentation
 #'
-#' @format The states database is a list that contains the
-#' following 3 datasets: GW, ISD, COW.
+#' @format The states database is a list that contains a shorter
+#' version of the following 3 datasets: GW, ISD, COW.
+#' @details These datasets are to be used for testing and
+#' running examples and vignettes in qData.
+#' For the complete database and more datasets on states,
+#' please refer to the `{qStates}` package. 
 #'\describe{
 #' \item{GW: }{A dataset with 150 observations and the following 5 variables: COW_Nr, ID, Beg, End, Label.}
 #' \item{ISD: }{A dataset with 150 observations and the following 7 variables: COW_Nr, ID, Beg, End, Label, Micro, New.State.}
