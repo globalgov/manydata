8123d10139c7e9ff6f6301a78fc3c9972dd22be8
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-23 12:50:09 +0200

    Renamed and updated emperors documentation script

---
+++ R/emperors_database.R
@@ -0,0 +1,17 @@
+#' emperors database documentation
+#'
+#' @format The emperors database is a list that contains the
+#' following 3 datasets: wikipedia, UNRV, britannica.
+#' For more information and references to each of the datasets used,
+#' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
+#' functions.
+#'\describe{
+#' \item{wikipedia: }{A dataset with 68 observations and the following 16 variables:
+#' index, name.full, birth, death, birth.cty, birth.prv, rise, cause,
+#' killer, dynasty, era, notes, verif.who, ID, Beg, End.}
+#' \item{UNRV: }{A dataset with 99 observations and the following 7 variables:
+#' Beg, End, ID, Birth, Death, Full Name/Imperial Name, Dynasty/Class/Notes.}
+#' \item{britannica: }{A dataset with 87 observations and the following 3 variables:
+#' ID, Beg, End.}
+#' }
+ "emperors"
