ac118a3f5b4c4dfbc64673a7e97bd175ecdb1c8f
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-23 18:15:15 +0200

    Minor documentation updates to emperors_database documentation

--- R/emperors_database.R
+++ R/emperors_database.R
@@ -8,9 +8,9 @@
 #'\describe{
 #' \item{wikipedia: }{A dataset with 68 observations and the following 16 variables:
 #' Index, FullName, Birth, Death, CityBirth, ProvinceBirth, Rise, Cause, Killer,
-#' Dynasty, Era, Notes, Verif, ID, Beg, End
+#' Dynasty, Era, Notes, Verif, ID, Beg, End.}
 #' \item{UNRV: }{A dataset with 99 observations and the following 7 variables:
-#' Beg, End, ID, Birth, Death, FullName, Dynastydevt.}
+#' Beg, End, ID, Birth, Death, FullName, Dynasty.}
 #' \item{britannica: }{A dataset with 87 observations and the following 3 variables:
 #' ID, Beg, End.}
 #' }
