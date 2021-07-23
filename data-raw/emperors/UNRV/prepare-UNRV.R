1ca6ab8f65da8a8716ac98085e8b362ebdddbb8c
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-23 12:48:56 +0200

    Updated preparation script for UNRV dataset

--- data-raw/emperors/UNRV/prepare-UNRV.R
+++ data-raw/emperors/UNRV/prepare-UNRV.R
@@ -15,52 +15,27 @@ UNRV <- readxl::read_excel("data-raw/emperors/UNRV/UNRV.xlsx")
 # In this stage you will want to correct the variable names and
 # formats of the 'UNRV' object until the object created
 # below (in stage three) passes all the tests.
-
-# Add the 00 to have a date format compatible with other datasets in emperors database
+# First, let's just complete year variables so that Beg and End variables
+# have at least have 4 digits for years.
 for(i in c(1:13)) {
   UNRV$Beg[i] <- paste0("00", UNRV$Beg[i])
 }
-
 for(i in c(14:99)) {
   UNRV$Beg[i] <- paste0("0", UNRV$Beg[i])
 }
-
 for(i in c(1:12)) {
   UNRV$End[i] <- paste0("00", UNRV$End[i])
 }
-
 for(i in c(13:99)) {
   UNRV$End[i] <- paste0("0", UNRV$End[i])
 }
-
-for(i in c(1:5, 7,8,10:15)) {
-  UNRV$Birth[i] <- paste0("00", UNRV$Birth[i])
-}
-
-for(i in c(6,9)) {
-  UNRV$Birth[i] <- paste0("000", UNRV$Birth[i])
-}
-
-for(i in c(16:40, 46,48,51,52, 55, 59, 60, 62:75, 77, 79, 80, 81, 83,84, 89, 91:96, 99)) {
-  UNRV$Birth[i] <- paste0("0", UNRV$Birth[i])
-}
-
-for(i in c(1:12)) {
-  UNRV$Death[i] <- paste0("00", UNRV$Death[i])
-}
-
-for(i in c(13:87, 89:99)) {
-  UNRV$Death[i] <- paste0("0", UNRV$Death[i])
-}
-
-
+# standardise_dates() is a wrapper function for
+# messydates::as_messydates() and messydates::make_messydates(). 
+# Let's standardise dates and variable names
 UNRV <- as_tibble(UNRV) %>%
-  dplyr::mutate(Beg = as_messydate(Beg)) %>% 
-  dplyr::mutate(End = as_messydate(End)) %>% 
-  dplyr::mutate(Birth = as_messydate(Birth)) %>% 
-  dplyr::mutate(Death = as_messydate(Death)) %>% 
-  transmutate(ID = `Common Name`) %>%
-  dplyr::arrange(Beg)
+  dplyr::mutate(Beg = qCreate::standardise_dates(Beg),
+                End = qCreate::standardise_dates(End)) %>% 
+  rename(ID = "Common Name")
 # qData includes several functions that should help cleaning
 # and standardising your data.
 # Please see the vignettes or website for more details.
@@ -75,7 +50,7 @@ export_data(UNRV, database = "emperors", URL = "https://www.unrv.com/government/
 # to run these tests locally at any point.
 # Any test failures should be pretty self-explanatory and may require
 # you to return to stage two and further clean, standardise, or wrangle
-# your data into the expected format.
+# your data into the expected format.
 # Second, it also creates a documentation file for you to fill in.
 # Please make sure that you cite any sources appropriately and fill in as
 # much detail about the variables etc as possible.
