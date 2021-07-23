3afca790a5f4c18a9d69e9843bb753a9d081e17c
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-23 18:06:15 +0200

    Upadted database documentation for changes in variable names

--- R/emperors_database.R
+++ R/emperors_database.R
@@ -7,10 +7,10 @@
 #' functions.
 #'\describe{
 #' \item{wikipedia: }{A dataset with 68 observations and the following 16 variables:
-#' index, name.full, birth, death, birth.cty, birth.prv, rise, cause,
-#' killer, dynasty, era, notes, verif.who, ID, Beg, End.}
+#' Index, FullName, Birth, Death, CityBirth, ProvinceBirth, Rise, Cause, Killer,
+#' Dynasty, Era, Notes, Verif, ID, Beg, End
 #' \item{UNRV: }{A dataset with 99 observations and the following 7 variables:
-#' Beg, End, ID, Birth, Death, Full Name/Imperial Name, Dynasty/Class/Notes.}
+#' Beg, End, ID, Birth, Death, FullName, Dynastydevt.}
 #' \item{britannica: }{A dataset with 87 observations and the following 3 variables:
 #' ID, Beg, End.}
 #' }
