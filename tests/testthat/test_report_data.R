b98dc6d0020d217c256a9f26895bd766e885ec63
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-23 18:24:13 +0200

    Updated tests for report_data() function with new emperors database

--- tests/testthat/test_report_data.R
+++ tests/testthat/test_report_data.R
@@ -1,11 +1,11 @@
 test_that("data_source() returns the correct output format", {
   expect_type(data_source("qData"), "list")
-  expect_type(data_source("qData", "states"), "list")
-  expect_type(data_source("qData", "states", "COW"), "list")
+  expect_type(data_source("qData", "emperors"), "list")
+  expect_type(data_source("qData", "emperors", "wikipedia"), "list")
 })
 
 test_that("data_contrast() returns the correct output format", {
-  expect_type(data_contrast("qData", "states"), "list")
-  expect_type(data_contrast("qData", "states", "COW"), "list")
-  expect_invisible(contrast <- data_contrast("qData", "states", "COW"))
+  expect_type(data_contrast("qData"), "list")
+  expect_type(data_contrast("qData", "emperors"), "list")
+  expect_type(data_contrast("qData", "emperors", "wikipedia"), "list")
 })
