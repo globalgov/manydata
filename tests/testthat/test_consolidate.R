99c40716b60c7234eb2d882ca103253a8790da25
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-11-10 11:01:58 +0100

    Updated tests for consolidate() for new changes with function

--- tests/testthat/test_consolidate.R
+++ tests/testthat/test_consolidate.R
@@ -31,14 +31,18 @@ data.dup <- dplyr::tibble(qID = c("NZL", "BRA", "CHF", "OTH",
                                    "1990-01-01:1990-01-31", NA),
                           number = c(100, 1000, 10000, 21,
                                      100, 1000, 10000, 21))
+data.con.min <- dplyr::tibble(qID = c("NZL", "BRA"),
+                              date = c("1990-01-01",
+                                       "1990-01-02"),
+                              number = c("100", "1000"))
 data.con.max <- dplyr::tibble(qID = c("NZL", "BRA"),
                               date = c("1990-01-01",
                                        "1990-01-03"),
-                              number = c(100, 1200))
+                              number = c("100", "1200"))
 data.con.median <- dplyr::tibble(qID = c("NZL", "BRA"),
                               date = c("1990-01-01",
                                        "1990-01-02"),
-                              number = c(100, 1100))
+                              number = c("100", "1100"))
 
 test_that("pluck works", {
   expect_equal(pluck(test, "a"), data1)
@@ -51,12 +55,13 @@ test_that("coalesce_compatible works", {
   expect_equal(coalesce_compatible(data.dup), data.13.any)
 })
 
-test_that("consolidate works", {
+test_that("consolidate methods", {
   expect_equal(consolidate(test, "every", "every", resolve = "coalesce"), data.con.con)
   expect_equal(consolidate(test, "every", "any", resolve = "coalesce"), data.con.any)
   expect_equal(consolidate(test[c(1, 3)], "any", "any", resolve = "coalesce"), data.13.any)
   expect_equal(consolidate(test, "any", resolve = "coalesce"), data.13.any)
-  expect_equal(consolidate(test, "every", "any", resolve = "min"), data.con.any)
+  expect_equal(consolidate(test, "every", "any", resolve = "min"), data.con.min)
   expect_equal(consolidate(test, "every", "any", resolve = "max"), data.con.max)
   expect_equal(consolidate(test, "every", "any", resolve = "median"), data.con.median)
+  expect_equal(consolidate(test, "every", "any", resolve = "mean"), data.con.median)
 })
