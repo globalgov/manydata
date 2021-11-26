5a68ee6fdaf5e97edee4ed6a4169537691df9f68
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-11-26 11:18:05 +0100

    Updated extract function documentation and addressed github check notes

--- R/extract_bilaterals.R
+++ R/extract_bilaterals.R
@@ -1,13 +1,28 @@
-#' Extracting adjacency edgelist from bilaterals in a mixed edgelist
+#' Extracting adjacency edgelist for international agreements
+#' 
+#' @param membs A memberships dataset from one of the many packages 
+#' @description The set of functions help
+#' extracting adjacency edgelist for bilaterals agreements in a mixed edgelist,
+#' and extracting adjacency edgelist for multilateral agreements
+#' in a mixed edgelist.
+#' These membership edgelist can be found in memebership databases across
+#' the many package universe.
 #' @name extraction
+NULL
+
+#' @name extraction
+#' @details The function helps extracting adjacency edgelist
+#' for bilaterals agreements in a mixed edgelist.
+#' @import dplyr
+#' @return An edgelist for bilaterals agreements
 #' @examples 
 #' \dontrun{
-#' membs <- qEnviron::memberships$IEADB_MEM
+#' membs <- manyenviron::memberships$IEADB_MEM
 #' extract_bilaterals(membs)
-#' extract_multilaterals(membs)
 #' }
 #' @export
 extract_bilaterals <- function(membs){
+  Beg <- CountryID <- CountryID1 <- CountryID2 <- End <- Title <- qID_ref <- NULL
   bilats <- subset(membs, grepl("[A-Z]{3}-", qID_ref)) %>% 
     dplyr::arrange(qID_ref, CountryID) %>% 
     dplyr::select(CountryID, qID_ref, Title, Beg, End)
@@ -22,9 +37,19 @@ extract_bilaterals <- function(membs){
   bilats
 }
 
-#' @rdname extraction
+#' @name extraction
+#' @details The function helps extracting adjacency edgelist
+#' for multilateral agreements in a mixed edgelist.
+#' @importFrom dplyr filter
+#' @return An edgelist for multilateral agreements
+#' @examples
+#' \dontrun{
+#' membs <- manyenviron::memberships$IEADB_MEM
+#' extract_multilaterals(membs)
+#' }
 #' @export
 extract_multilaterals <- function(membs){
-  subset(membs, !grepl("[A-Z]{3}-", qID_ref)) %>% 
+  qID_ref <- NULLref <- NULL
+  subset(membs, !grepl("[A-Z]{3}-", NULLref)) %>% 
     dplyr::filter(qID_ref %in% names(table(qID_ref)[table(qID_ref)!=2]))
 }
