27fd7b0b87677dd10c3c3b86ac9feca3b7dedf19
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-12-01 12:00:51 +0100

    Added more tests for consolidate function

--- tests/testthat/test_consolidate.R
+++ tests/testthat/test_consolidate.R
@@ -44,16 +44,28 @@ data.con.median <- dplyr::tibble(many_ID = c("NZL", "BRA"),
                                        "1990-01-02"),
                               number = c("100", "1100"))
 
+# input data
+dat1 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF"),
+                      date = messydates::as_messydate(c("1990-01-01", "1990-01-02",
+                                                        "1990-01-01:1990-01-31")),
+                      number = c(100, 1000, 10000))
+dat2 <- dplyr::tibble(many_ID = c("NZL", "BRA"),
+                      date = messydates::as_messydate(c("1990-01-01", "1990-01-03")))
+dat3 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
+                      date = messydates::as_messydate(c("1990-01-01", "1990-01-02",
+                                                        "1990-01-01:1990-01-31", NA)),
+                       number = c(100, 1200, 12222, 21))
+test2 <- tibble::lst(a = dat1, b = dat2, c = dat3)
+
+
+
 data.multi <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
-                            date = c("1990-01-01", "1990-01-02",
-                                     "1990-01-01:1990-01-31", NA),
+                            date = c("1990-01-01", "1990-01-02", "1990-01-01", NA),
                             number = c("100", "1200", "12222", "21"))
 
 data.many <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
                            number = c("100", "1100", "11111", "21"),
-                           date = c("1990-01-01", "1990-01-02",
-                                    "1990-01-01:1990-01-31", NA))
-
+                           date = c("1990-01-01", "1990-01-02", "1990-01-01..1990-01-31", NA))
 
 test_that("pluck works", {
   expect_equal(pluck(test, "a"), data1)
@@ -76,7 +88,12 @@ test_that("consolidate methods", {
   expect_equal(consolidate(test, "every", "any", resolve = "median"), data.con.median)
   expect_equal(consolidate(test, "every", "any", resolve = "mean"), data.con.median)
   expect_length(consolidate(test, "every", "any", resolve = "random"), 3)
-  expect_equal(consolidate(test, "any", "any", resolve = c(date = "min", number = "max")), data.multi)
-  expect_equal(consolidate(test, "any", "any", resolve = c(date = "mean", number = "median")), data.many)
-  expect_length(consolidate(test, "any", "any", resolve = c(date = "coalesce", number = "random")), 3)
+  expect_equal(consolidate(test2, "every", "any", resolve = "min"), data.con.min)
+  expect_equal(consolidate(test2, "every", "any", resolve = "max"), data.con.max)
+  expect_equal(consolidate(test2, "every", "any", resolve = "median"), data.con.median)
+  expect_equal(consolidate(test2, "every", "any", resolve = "mean"), data.con.median)
+  expect_length(consolidate(test2, "every", "any", resolve = "random"), 3)
+  expect_equal(consolidate(test2, "any", "any", resolve = c(date = "min", number = "max")), data.multi)
+  expect_equal(consolidate(test2, "any", "any", resolve = c(date = "mean", number = "median")), data.many)
+  expect_length(consolidate(test2, "any", "any", resolve = c(date = "coalesce", number = "random")), 3)
 })
