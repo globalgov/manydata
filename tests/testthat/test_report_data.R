32d72b8db682e234e53bd8cd48266d080256e8cd
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-12-01 11:38:43 +0100

    Added more tests for data report functions

--- tests/testthat/test_report_data.R
+++ tests/testthat/test_report_data.R
@@ -1,11 +1,17 @@
 test_that("data_source() returns the correct output format", {
   expect_type(data_source("manydata"), "list")
+  expect_length(data_source("manydata"), 1)
   expect_type(data_source("manydata", "emperors"), "list")
+  expect_length(data_source("manydata", "emperors"), 1)
   expect_type(data_source("manydata", "emperors", "wikipedia"), "list")
+  expect_length(data_source("manydata", "emperors", "wikipedia"), 1)
 })
 
 test_that("data_contrast() returns the correct output format", {
   expect_type(data_contrast("manydata"), "list")
+  expect_length(data_contrast("manydata"), 1)
   expect_type(data_contrast("manydata", "emperors"), "list")
+  expect_length(data_contrast("manydata", "emperors"), 1)
   expect_type(data_contrast("manydata", "emperors", "wikipedia"), "list")
+  expect_length(data_contrast("manydata", "emperors", "wikipedia"), 1)
 })
