0bc564acacac20b899e07ca24b49d3c25b5338d6
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-12-03 09:51:51 +0100

    Updated tests for plot_releases function

--- tests/testthat/test_plot_releases.R
+++
@@ -1,6 +0,0 @@
-test_that("plot_releases work properly", {
-  expect_error(plot_releases(), "argument \"repo\" is missing, with no default")
-  a <- plot_releases("globalgov/manydata")
-  expect_type(a, "list")
-  expect_length(a, 9)
-})
