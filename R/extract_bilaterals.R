7de9ed432ae40b364d61641281ecdb081ff3c665
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-25 12:55:19 +0100

    Updated examples for extraction functions not to call manyenviron by adding a short tibble to examples

--- R/extract_bilaterals.R
+++ R/extract_bilaterals.R
@@ -15,21 +15,27 @@ NULL
 #' @import dplyr
 #' @return An edgelist for bilaterals agreements
 #' @examples
-#' \dontrun{
-#' membs <- manyenviron::memberships$IEADB_MEM
+#' membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
+#' manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
+#' Title = c("Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
+#' "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
+#' "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain
+#' And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside
+#' Territorial Waters In The Ocean Surrounding The Faroe Islands"),
+#' Beg = c("1901-02-22", "1901-02-22", "1901-06-24"),
+#' End = c(NA, NA, NA))
 #' extract_bilaterals(membs)
-#' }
 #' @export
 extract_bilaterals <- function(membs) {
-  Beg <- CountryID <- CountryID1 <- CountryID2 <- End <- Title <- many_ID <- NULL
-  if (!any(colnames(membs) == "many_ID")) {
-    stop("many_ID column not found, please declare a many packages dataset.")
+  Beg <- CountryID <- CountryID1 <- CountryID2 <- End <- Title <- manyID <- NULL
+  if (!any(colnames(membs) == "manyID")) {
+    stop("manyID column not found, please declare a many packages dataset.")
   }
-  bilats <- subset(membs, grepl("[A-Z]{3}-", many_ID)) %>%
-    dplyr::arrange(many_ID, CountryID) %>%
-    dplyr::select(CountryID, many_ID, Title, Beg, End)
+  bilats <- subset(membs, grepl("[A-Z]{3}-", manyID)) %>%
+    dplyr::arrange(manyID, CountryID) %>%
+    dplyr::select(CountryID, manyID, Title, Beg, End)
   bilats <- bilats %>%
-    dplyr::filter(many_ID %in% names(table(bilats$many_ID)[table(bilats$many_ID) == 2]))
+    dplyr::filter(manyID %in% names(table(bilats$manyID)[table(bilats$manyID) == 2]))
   bilats1 <- bilats %>%
     dplyr::filter(row_number() %% 2 == 0) %>%
     dplyr::rename(CountryID1 = "CountryID")
@@ -37,7 +43,7 @@ extract_bilaterals <- function(membs) {
     dplyr::filter(row_number() %% 2 == 1) %>%
     dplyr::rename(CountryID2 = "CountryID")
   bilats <- dplyr::full_join(bilats2, bilats1,
-                             by = c("many_ID", "Title", "Beg", "End")) %>%
+                             by = c("manyID", "Title", "Beg", "End")) %>%
     dplyr::select(CountryID1, CountryID2, Title, Beg, End)
   bilats
 }
@@ -49,18 +55,24 @@ extract_bilaterals <- function(membs) {
 #' @importFrom stringr str_detect
 #' @return An edgelist for multilateral agreements
 #' @examples
-#' \dontrun{
-#' membs <- manyenviron::memberships$IEADB_MEM
+#' membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
+#' manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
+#' Title = c("Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
+#' "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
+#' "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain
+#' And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside
+#' Territorial Waters In The Ocean Surrounding The Faroe Islands"),
+#' Beg = c("1901-02-22", "1901-02-22", "1901-06-24"),
+#' End = c(NA, NA, NA))
 #' extract_multilaterals(membs)
-#' }
 #' @export
 extract_multilaterals <- function(membs) {
-  many_ID <- Title <- Beg <- End <- NULL
-  if (!any(colnames(membs) == "many_ID")) {
-    stop("many_ID column not found, please declare a many packages dataset.")
+  manyID <- Title <- Beg <- End <- NULL
+  if (!any(colnames(membs) == "manyID")) {
+    stop("manyID column not found, please declare a many packages dataset.")
   }
-  multi <- subset(membs, stringr::str_detect(many_ID, "\\-", negate = TRUE)) %>%
-    dplyr::filter(many_ID %in% names(table(many_ID)[table(many_ID) != 2])) %>%
-    dplyr::select(many_ID, Title, Beg, End)
+  multi <- subset(membs, stringr::str_detect(manyID, "\\-", negate = TRUE)) %>%
+    dplyr::filter(manyID %in% names(table(manyID)[table(manyID) != 2])) %>%
+    dplyr::select(manyID, Title, Beg, End)
   multi
 }
