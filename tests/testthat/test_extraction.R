3d88246e6cfc5b2dd7971e9b3683abf71834182b
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-25 12:58:27 +0100

    Updated variable names in extraction functions to reflect new many naming conventions. Tests were also updated.

--- tests/testthat/test_extraction.R
+++ tests/testthat/test_extraction.R
@@ -1,5 +1,5 @@
 membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
-                        many_ID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
+                        manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
                         Title = c("Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
                                   "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
                                   "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside Territorial Waters In The Ocean Surrounding The Faroe Islands"),
@@ -8,7 +8,7 @@ membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
 bilats <- tibble::tibble(CountryID1 = "RUS", CountryID2 = "ROU",
                           Title = "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
                           Beg = "1901-02-22", End = NA)
-multi <- tibble::tibble(many_ID = "GD16FI_1901A",
+multi <- tibble::tibble(manyID = "GD16FI_1901A",
                         Title = "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside Territorial Waters In The Ocean Surrounding The Faroe Islands",
                         Beg = "1901-06-24", End = NA)
 
