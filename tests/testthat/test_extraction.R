d958f3e23cef6f22a8a63972970ef2de525beac3
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-28 12:13:28 +0100

    Simplified tests for extraction functions

--- tests/testthat/test_extraction.R
+++ tests/testthat/test_extraction.R
@@ -1,15 +1,15 @@
 membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
                         manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
-                        Title = c("Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
-                                  "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
-                                  "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside Territorial Waters In The Ocean Surrounding The Faroe Islands"),
+                        Title = c("Convention Between Roumania And Russia Concerning Fishing",
+                                  "Convention Between Roumania And Russia Concerning Fishing",
+                                  "Convention Between The Governments Of Denmark And ..."),
                         Beg = c("1901-02-22", "1901-02-22", "1901-06-24"),
                         End = c(NA, NA, NA))
 bilats <- tibble::tibble(CountryID1 = "RUS", CountryID2 = "ROU",
-                          Title = "Convention Between Roumania And Russia Concerning Fishing In The Danube And The Pruth",
+                          Title = "Convention Between Roumania And Russia Concerning Fishing",
                           Beg = "1901-02-22", End = NA)
 multi <- tibble::tibble(manyID = "GD16FI_1901A",
-                        Title = "Convention Between The Governments Of Denmark And The United Kingdom Of Great Britain And Northern Ireland For Regulating The Fisheries Of Their Respective Subjects Outside Territorial Waters In The Ocean Surrounding The Faroe Islands",
+                        Title = "Convention Between The Governments Of Denmark And ...",
                         Beg = "1901-06-24", End = NA)
 
 test_that("extract functions work", {
