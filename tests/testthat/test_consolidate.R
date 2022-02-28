ddf86435725c7fffbf5c106bb65f6103420dc049
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-28 12:13:02 +0100

    Re-arranged consolidate test script for consistency

--- tests/testthat/test_consolidate.R
+++ tests/testthat/test_consolidate.R
@@ -11,6 +11,22 @@ data3 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
                        number = c(100, 1200, 12222, 21))
 test <- tibble::lst(a = data1, b = data2, c = data3)
 
+dat1 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF"),
+                      date = messydates::as_messydate(c("1990-01-01",
+                                                        "1990-01-02",
+                                                        "1990-01-01:1990-01-31")),
+                      number = c(100, 1000, 10000))
+dat2 <- dplyr::tibble(many_ID = c("NZL", "BRA"),
+                      date = messydates::as_messydate(c("1990-01-01",
+                                                        "1990-01-03")))
+dat3 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
+                      date = messydates::as_messydate(c("1990-01-01",
+                                                        "1990-01-02",
+                                                        "1990-01-01:1990-01-31",
+                                                        NA)),
+                      number = c(100, 1200, 12222, 21))
+test2 <- tibble::lst(a = dat1, b = dat2, c = dat3)
+
 # expected output data
 data.con.con <- dplyr::tibble(many_ID = c("NZL", "BRA"),
                                 date = c("1990-01-01",
@@ -43,31 +59,10 @@ data.con.median <- dplyr::tibble(many_ID = c("NZL", "BRA"),
                               date = c("1990-01-01",
                                        "1990-01-02"),
                               number = c("100", "1100"))
-
-# input data
-dat1 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF"),
-                      date = messydates::as_messydate(c("1990-01-01",
-                                                        "1990-01-02",
-                                                        "1990-01-01:1990-01-31")),
-                      number = c(100, 1000, 10000))
-dat2 <- dplyr::tibble(many_ID = c("NZL", "BRA"),
-                      date = messydates::as_messydate(c("1990-01-01",
-                                                        "1990-01-03")))
-dat3 <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
-                      date = messydates::as_messydate(c("1990-01-01",
-                                                        "1990-01-02",
-                                                        "1990-01-01:1990-01-31",
-                                                        NA)),
-                       number = c(100, 1200, 12222, 21))
-test2 <- tibble::lst(a = dat1, b = dat2, c = dat3)
-
-
-
 data.multi <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
                             date = c("1990-01-01", "1990-01-02",
                                      "1990-01-01", NA),
                             number = c("100", "1200", "12222", "21"))
-
 data.many <- dplyr::tibble(many_ID = c("NZL", "BRA", "CHF", "OTH"),
                            number = c("100", "1100", "11111", "21"),
                            date = c("1990-01-01", "1990-01-02",
@@ -120,6 +115,6 @@ test_that("consolidate methods", {
                            resolve = c(date = "mean", number = "median")),
                data.many)
   expect_length(consolidate(test2, "any", "any",
-                            resolve = c(date = "coalesce", number = "random")),
-                3)
+                            resolve = c(date = "coalesce",
+                                        number = "random")), 3)
 })
