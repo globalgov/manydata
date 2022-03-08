a8a556dda1d4c735fe5ec83625793b48250a3aa7
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-03-08 15:44:29 +0100

    Changed descriptions for extraction functions to remove references to edgelists

--- R/extract.R
+++ R/extract.R
@@ -1,19 +1,17 @@
-#' Extracting adjacency edgelist for international agreements
+#' Extracting international agreements
+#' 
 #' @param membs A memberships dataset from one of the many packages
-#' @description The set of functions help
-#' extracting adjacency edgelist for bilaterals agreements in a mixed edgelist,
-#' and extracting adjacency edgelist for multilateral agreements
-#' in a mixed edgelist.
-#' These membership edgelists can be found in membership databases across
-#' the many package universe.
+#' @description This set of functions help extracting bilateral agreements or
+#' multilateral agreements from a membership database
+#' in the many packages universe.
 #' @name extraction
 NULL
 
 #' @name extraction
-#' @details The function helps extracting adjacency edgelist
-#' for bilaterals agreements in a mixed edgelist.
+#' @details The function helps extracting bilateral agreements
+#' from a membership database in the many packages universe.
 #' @import dplyr
-#' @return An edgelist for bilaterals agreements
+#' @return A tibble of bilateral agreements
 #' @examples
 #' membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
 #' manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
@@ -39,7 +37,8 @@ extract_bilaterals <- function(membs) {
     dplyr::arrange(manyID, CountryID) %>%
     dplyr::select(CountryID, manyID, Title, Beg, End)
   bilats <- bilats %>%
-    dplyr::filter(manyID %in% names(table(bilats$manyID)[table(bilats$manyID) == 2]))
+    dplyr::filter(manyID %in%
+                    names(table(bilats$manyID)[table(bilats$manyID) == 2]))
   bilats1 <- bilats %>%
     dplyr::filter(row_number() %% 2 == 0) %>%
     dplyr::rename(CountryID1 = "CountryID")
@@ -53,11 +52,11 @@ extract_bilaterals <- function(membs) {
 }
 
 #' @name extraction
-#' @details The function helps extracting adjacency edgelist
-#' for multilateral agreements in a mixed edgelist.
+#' @details The function helps extracting multilateral agreements
+#' from a membership database in the many packages universe.
 #' @importFrom dplyr filter
 #' @importFrom stringr str_detect
-#' @return An edgelist for multilateral agreements
+#' @return A tibble of multilateral agreements
 #' @examples
 #' membs <- tibble::tibble(CountryID = c("ROU", "RUS", "DNK"),
 #' manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
