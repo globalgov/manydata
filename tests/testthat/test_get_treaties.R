4dc0fc81d7c450df0554e126599bcce1b8b5aca8
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-07-22 17:26:39 +0200

    Updated tests and adde tests for new retrieve functions

--- tests/testthat/test_get_treaties.R
+++ tests/testthat/test_get_treaties.R
@@ -11,8 +11,17 @@ bilats <- tibble::tibble(CountryID1 = "RUS", CountryID2 = "ROU",
 multi <- tibble::tibble(manyID = "GD16FI_1901A",
                         Title = "Convention Between The Governments Of Denmark And ...",
                         Beg = "1901-06-24", End = NA)
+mlist <- tibble::tibble(manyID = c("ROU-RUS[RFP]_1901A", "GD16FI_1901A"),
+                       Memberships = c("ROU, RUS", "DNK"))
+db <- tibble::tibble(manyID = c("ROU-RUS[RFP]_1901A",
+                                "ROU-RUS[RFP]_1901A:ROU-RUS[RFP]_1901A",
+                                "GD16FI_1901A"))
+links <- tibble::tibble(Agreement = "ROU-RUS[RFP]_1901A",
+                        Link = "ROU-RUS[RFP]_1901A")
 
-test_that("extract functions work", {
-  expect_equal(get_bilaterals(membs), bilats)
-  expect_equal(get_multilaterals(membs), multi)
+test_that("retrieve functions work", {
+  expect_equal(retrieve_bilaterals(membs), bilats)
+  expect_equal(retrieve_multilaterals(membs), multi)
+  expect_equal(retrieve_membership_list(membs), mlist)
+  expect_equal(retrieve_links(dataset = db), links)
 })
