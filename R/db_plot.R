429c1a3b97ab6efe5461ad14f6c05633b63a0723
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-08-19 15:36:48 +0200

    Updated db_plot() function to properly handle NAs. Plot colors and style were also improved. A message with the number of pairwise matches is returned by the function now.

--- R/db_plot.R
+++ R/db_plot.R
@@ -12,9 +12,15 @@ db_plot <- function(database, key) {
   }
   # reduce database
   out <- purrr::reduce(database, dplyr::full_join, by = key)
-  # get variable names, but ID
+  # get the number of pairwise overlap between datasets by key variable
+  cat("There were", sum(duplicated(unname(unlist(purrr::map(database, key))))),
+      "matched observations by key variable across datasets in database.")
+  # get variable names, but key
   all_variables <- unname(unlist(purrr::map(database, names)))
   all_variables <- all_variables[!all_variables %in% key]
+  # remove other ID variables
+  ID_var <- grep("^ID|ID$", all_variables, value = TRUE)
+  all_variables <- all_variables[!all_variables %in% ID_var]
   # create an empty data frame
   db <- data.frame(out[,1], stringsAsFactors = TRUE)
   # check if values are missing, confirmed, conflicting, asymmetrical, or unique
@@ -26,23 +32,38 @@ db_plot <- function(database, key) {
       value <- apply(out[vars_to_combine], 1, paste, collapse = "!")
       # weird code added to some variables? need to double check messydates...
       value <- stringr::str_remove_all(value, "\032")
-      # remove string duplicates and collapse unique values
-      value <- sapply(stringr::str_split(value, "!"),
-                      function(x) {paste(unique(trimws(x)), collapse = '!')})
-      # work with fractions to determine category
-      value <- ifelse(stringr::str_detect(value, "^NA$|^na$|^NA_character$"),
-                      "missing", value)
-      value <- ifelse(stringr::str_count(value, "\\!") ==
-                        (length(out[vars_to_combine]) - 1), "conflict", value)
+      # remove string duplicates and collapse unique values (except NAs)
+      value <- sapply(stringr::str_split(value, "!"), function(x) {
+                        paste(unique(trimws(x), incomparables = "NA"),
+                              collapse = '!')
+        })
+      # missing
+      value <- ifelse(stringr::str_count(value, "NA") ==
+                        length(out[vars_to_combine]), "missing", value)
+      # unique
+      value <- ifelse(stringr::str_count(value, "NA") ==
+                        (length(out[vars_to_combine]) - 1), "unique", value)
+      # confirmed (by all datasets in database)
       value <- ifelse(stringr::str_count(value, "\\!") == 0 &
-                        !grepl("^missing$|^conflict$", value),
+                        !grepl("^missing$|^unique$", value),
                       "confirmed", value)
-      value <- ifelse(!grepl("^missing$|^conflict$|^confirmed$", value),
-                      "asymmetric", value)
-      # Questions:
-      # What to do with NAs when there are conflicts? 
-      # What to do with confirmed obs in multiple datasets but not all?
-      # What to do with with similar dates with different levels of precision?
+      # remove duplicated NAs
+      valuec <- sapply(stringr::str_split(value, "!"), function(x) {
+        paste(unique(trimws(x)), collapse = '!')
+        })
+      # confirmed (by multiple datasets in database, other values are NAs)
+      value <- ifelse(stringr::str_count(valuec, "\\!") == 1 &
+                        stringr::str_count(valuec, "\\!NA|NA\\!") == 1,
+                      "confirmed", value)
+      # conflict (when there are no duplicates apart from NAs)
+      value <- ifelse(stringr::str_count(value, "\\!") ==
+                        (length(out[vars_to_combine]) - 1),
+                      "conflict", value)
+      # asymmetric (assumes the rest is asymmetric for now)
+      # todo: fix this by "opening" where duplicated the variables are across
+      value <- ifelse(grepl("^missing$|^conflict$|^confirmed$|^unique$", value),
+                      value, "asymmetric")
+      # todo: what to do with similar dates with different levels of precision?
       } else {
         value <- out[vars_to_combine]
         value <- ifelse(is.na(value), "missing", "unique")
@@ -56,15 +77,20 @@ db_plot <- function(database, key) {
     tidyr::pivot_longer(cols = everything(), names_to = "variable",
                         values_to = "category") %>% 
     dplyr::group_by(variable, category) %>%
-    dplyr::summarise(count = n()) %>%
+    dplyr::summarise(count = n(), .groups = ) %>%
     dplyr::mutate(perc = count / sum(count)) %>% 
     tidyr::pivot_wider(id_cols = variable, names_from = category,
                        values_from = perc) %>% 
-    dplyr::mutate(across(everything(), ~tidyr::replace_na(.x, 0))) %>% 
-    tidyr::pivot_longer(asymmetric:confirmed) %>% 
+    dplyr::mutate(across(everything(), ~tidyr::replace_na(.x, 0))) %>%
+    tidyr::pivot_longer(-variable) %>% 
     dplyr::rename(Variables = variable, Category = name, Percentage = value)
+  # set colors
+  cols <- c(confirmed = 'Green', unique = 'Blue', missing = 'Beige',
+            conflict = 'Red', asymmetric = 'Orange')
   # plot
     ggplot(dbgather, aes(fill = Category, y = Percentage, x = Variables )) + 
-      geom_bar(position="fill", stat="identity") +
-      coord_flip()
+      geom_bar(position = "fill", stat = "identity") +
+      scale_fill_manual(values = cols) +
+      coord_flip() +
+      theme_minimal()
 }
