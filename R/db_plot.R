dde92041edc247e1b166175626e99de41280e02b
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-08-30 11:12:59 +0200

    Added dbcomp() function that compares values for coded categories across variable(s) within a many database.

--- R/db_plot.R
+++ R/db_plot.R
@@ -148,3 +148,110 @@ dbplot <- function(database, key = "manyID") {
          caption = "In between the parenthesis are the number of datasets in which variable is present.",
          x = "Variable")
 }
+
+#' Compare variable(s) in a 'many' database
+#'
+#' @param database A many database.
+#' @param variable One, or more, variables present in multiple datasets
+#' in a many database.
+#' For multiple variables, please declare as a list
+#' (i.e. c("variable1", "variable2"))
+#' @param key A variable key to join datasets by, "manyID" by default.
+#' @param category Would you like to focus on one specific code category?
+#' By default "all" are returned.
+#' Other options include "confirmed", "unique", "missing", "conflicting",
+#' or "majority".
+#' @details The tibble returns the values for all observations
+#' in the variable(s) selected from a 'many' package databasethat belong to all,
+#' or a specific, code category.
+#' Code categories include:
+#' Confirmed values are the same in all datasets in database.
+#' Unique values appear once in datasets in database.
+#' Missing values are missing in all datasets in database.
+#' Conflicting values are different in the same number of datasets in database.
+#' majority values have the same value in multiple, but not all,
+#' datasets in database.
+#' @importFrom dplyr full_join filter_all
+#' @importFrom purrr reduce
+#' @importFrom tibble tibble
+#' @importFrom stringr str_count str_remove_all str_split
+#' @return A tibble with the varaible(s) profile.
+#' @examples
+#' dbcomp(database = emperors, variable = "Beg", key = "ID")
+#' dbcomp(database = emperors, variable = c("Beg", "End"), key = "ID",
+#' category = "conflict")
+#' #dbcomp(database = manyenviron::agreements, variable = "Title",
+#' #category = "conflict")
+#' @export
+dbcomp <- function(database, variable, key = "manyID", category = "all") {
+  # Step 1: reduce data
+  out <- purrr::reduce(database, dplyr::full_join, by = key)
+  # get variable(s) of interest
+  all_variables <- unname(unlist(purrr::map(database, names)))
+  all_variables <- all_variables[all_variables %in% variable]
+  # create an empty data frame in case there is multiple variables
+  db <- data.frame(out[,1], stringsAsFactors = TRUE)
+  # Step 2: code variables
+  db <- data.frame(out[,1], stringsAsFactors = TRUE)
+  for (var in all_variables) {
+    vvars <- paste0("^", var, "$|^", var, "\\.")
+    vars_to_combine <- grepl(vvars, names(out))
+    vlb <- out[vars_to_combine]
+    colnames(vlb) <- paste0(names(database), "$", var)
+    if (length(vlb) < 1) {
+      stop("Please select a variable present in at least 2 datasets")
+    } else {
+      vl <- apply(vlb, 1, paste, collapse = "!") 
+      # weird code added to some variables? need to double check messydates...
+      vl <- stringr::str_remove_all(vl, "\032")
+      # remove string duplicates and collapse unique values (except NAs)
+      # todo: is unique() slow for lists?
+      value <- unlist(lapply(stringr::str_split(vl, "!"), function(x) {
+        paste(unique(trimws(x), incomparables = "NA"), collapse = '!')
+      }))
+      # missing
+      value <- ifelse(stringr::str_count(value, "NA") ==
+                        length(out[vars_to_combine]), "missing", value)
+      # unique
+      value <- ifelse(stringr::str_count(value, "NA") ==
+                        (length(out[vars_to_combine]) - 1), "unique", value)
+      # confirmed (by all datasets in database)
+      value <- ifelse(stringr::str_count(value, "\\!") == 0 &
+                        !grepl("^missing$|^unique$", value),
+                      "confirmed", value)
+      # confirmed (by multiple datasets in database, other values are NAs)
+      valuec <- lapply(stringr::str_split(value, "!"), function(x) {
+        x[!grepl("^NA$", x) & x != ""]
+      })
+      value <- ifelse(lengths(valuec) == 1 &
+                        !grepl("^missing$|^unique$|^confirmed$", valuec),
+                      "confirmed", value)
+      # conflict (when there are no duplicates, apart from NAs possibly)
+      value <- ifelse(!grepl("^missing$|^unique$|^confirmed$", value) &
+                        stringr::str_count(value, "\\!") ==
+                        (length(out[vars_to_combine]) - 1),
+                      "conflict", value)
+      # open (and close) the values to find if majority or conflict
+      vl <- lapply(stringr::str_split(vl, "!"), function(x) {
+        x <- x[!grepl("^NA$", x) & x != ""]
+        x <- unique(rle(x)$lengths)
+        x
+      })
+      # conflict (an even number of non-NA conflicting obs)
+      value <- ifelse(!grepl("^missing$|^unique$|^confirmed$|^conflict$", value) &
+                        lengths(vl) == 1, "conflict", value)
+      # majority (an unbalanced number of non-NA matching obs)
+      value <- ifelse(!grepl("^missing$|^unique$|^confirmed$|^conflict$", value),
+                      "majority", value)
+    }
+    db <- cbind(db, vlb)
+    db[, var] <- value
+  }
+  db <- tibble::tibble(db[unique(colnames(db))])
+  # Step 3: filter categories if necessary
+  . <- NULL
+  if (category != "all") {
+    db <- dplyr::filter_all(db, any_vars(grepl(category, .)))
+  }
+  db
+}
