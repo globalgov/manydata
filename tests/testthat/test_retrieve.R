40a5baad2953b185698cf9348bed9c87e2fd6b10
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-11-17 11:17:27 +0100

    Added tests for new functions

--- tests/testthat/test_retrieve.R
+++ tests/testthat/test_retrieve.R
@@ -4,7 +4,8 @@ membs <- tibble::tibble(StateID = c("ROU", "RUS", "DNK"),
                                   "Convention Between Roumania And Russia Concerning Fishing",
                                   "Convention Between The Governments Of Denmark And ..."),
                         Beg = c("1901-02-22", "1901-02-22", "1901-06-24"),
-                        End = c(NA, NA, NA))
+                        End = c(NA, NA, NA),
+                        Text = c("treaty 1", "treaty 2", "treaty 3"))
 bilats <- tibble::tibble(manyID = "ROU-RUS[RFP]_1901A", StateID1 = "ROU", StateID2 = "RUS",
                          Title = "Convention Between Roumania And Russia Concerning Fishing",
                          Beg = "1901-02-22")
@@ -19,9 +20,14 @@ db <- tibble::tibble(manyID = c("ROU-RUS[RFP]_1901A",
 links <- tibble::tibble(Agreement = "ROU-RUS[RFP]_1901A",
                         Link = "ROU-RUS[RFP]_1901A")
 
+texts <- tibble::tibble(manyID = c("ROU-RUS[RFP]_1901A", "ROU-RUS[RFP]_1901A",
+                                   "GD16FI_1901A"),
+                        Text = c("treaty 1", "treaty 2", "treaty 3"))
+
 test_that("retrieve functions work", {
   expect_equal(retrieve_bilaterals(membs), bilats)
   expect_equal(retrieve_multilaterals(membs), multi)
   expect_equal(retrieve_membership_list(membs), mlist)
   expect_equal(retrieve_links(dataset = db), links)
+  expect_equal(retrieve_texts(dataset = membs), texts)
 })
