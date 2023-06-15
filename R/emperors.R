7b61e00b438fe42a7fa38796ee076237559ec46d
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-06-15 09:56:47 +0200

    Updated emperors documentation to test new documentation format for databases

--- R/emperors.R
+++ R/emperors.R
@@ -17,6 +17,81 @@
 #' \item{britannica: }{A dataset with 87 observations and the
 #' following 3 variables: ID, Beg, End.}
 #' }
+#' @source
+#' \itemize{
+#' \item{wikipedia: }{
+#' Wikipedia, List_of_Roman_emperors,
+#' \url{https://en.wikipedia.org/wiki/List_of_Roman_emperors}, Accessed on 2021-07-22
+#' }
+#' \item{UNRV: }{
+#' UNRV, Roman Emperor list,
+#' \url{https://www.unrv.com/government/emperor.php},
+#' Accessed on 2021-07-22
+#' }
+#' \item{britannica: }{
+#' Britannica, List of Roman emperors,
+#' \url{https://www.britannica.com/topic/list-of-Roman-emperors-2043294},
+#' Accessed on 2021-07-22
+#' }
+#' }
+#' @section URL:
+#' \itemize{
+#' \item{wikipedia: }{
+#' \url{https://en.wikipedia.org/wiki/List_of_Roman_emperors}
+#' }
+#' \item{UNRV: }{
+#' \url{https://www.unrv.com/government/emperor.php}
+#' }
+#' \item{britannica: }{
+#' \url{https://www.britannica.com/topic/list-of-Roman-emperors-2043294}
+#' }
+#' }
+#' @section Mapping:
+#' \itemize{
+#' \item{wikipedia: }{
+#' Variable Mapping
+#' 
+#' |  *from*  | *to*
+#' |:------------:|:------------:|
+#' | name | ID |
+#' | reign.start | Beg |
+#' | reign.end | End |
+#' | name.full | FullName |
+#' | birth | Birth |
+#' | death | Death |
+#' | birth.cty | CityBirth |
+#' | birth.prv | ProvinceBirth |
+#' | rise | Rise |
+#' | cause | Cause |
+#' | killer | Killer |
+#' | dynasty | Dynasty |
+#' | era | Era |
+#' | notes | Notes |
+#' | verif.who | Verif |
+#' 
+#' }
+#' \item{UNRV: }{
+#' Variable Mapping
+#' 
+#' |  *from*  | *to*
+#' |:------------:|:------------:|
+#' | Common Name | ID |
+#' | 'Full Name/Imperial Name' | FullName |
+#' | 'Dynasty/Class/Notes' | Dynasty |
+#' 
+#' }
+#' \item{britannica: }{
+#' Variable Mapping
+#' 
+#' |  *from*  | *to*
+#' |:------------:|:------------:|
+#' | Name | ID |
+#' | reign_start | Beg |
+#' | reign_end | End |
+#' 
+#' }
+#' }
+#' @md
 #' @details
 #' ``` {r, echo = FALSE, warning = FALSE}
 #' lapply(emperors, messydates::mreport)
