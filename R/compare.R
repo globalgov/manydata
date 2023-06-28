3bd2cef1b552b6a4cbd46a60ef9f6cdbe0998362
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-06-28 18:08:03 +0200

    Added 'plot' methods for `compare_` family of functions

--- R/compare.R
+++ R/compare.R
@@ -1,56 +1,23 @@
-#' Compare 'many' databases and datasets
+#' Compare data summaries for 'many' data
 #'
-#' @description
-#' The `compare_` functions in `{manydata}` allows users to quickly compare
-#' different information on databases and/or datasets across 'many packages'.
-#' These include comparison for:
-#' - data summaries
-#' - missing observations
-#' - categories
-#' Each of these functions is accompanied by an appropriate printing method that
-#' allows users to visualize the comparisons.
-#' @name compare
+#' @details `compare_data()` compares the number of observations, variables,
+#' the earliest and latest date in each dataset in a 'many' database.
+#' @family compare_
 #' @param database A database from one of the many packages.
 #' @param dataset A dataset in a database from one of the many packages.
 #' NULL by default.
 #' That is, all datasets in the database are used.
 #' A list of 2 or more datasets present in the database.
-#' @param key A variable key to join datasets.
-#' 'manyID' by default.
-#' @param variable Would you like to focus on one, or more, specific variables
-#' present in one or more datasets in the 'many' database?
-#' By default "all".
-#' For multiple variables, please declare variable names as a vector.
-#' @param category Would you like to focus on one specific code category?
-#' By default "all" are returned.
-#' Other options include "confirmed", "unique", "missing", "conflict",
-#' or "majority".
-#' For multiple variables, please declare categories as a vector.
-#' @importFrom dplyr as_tibble %>%
+#' @importFrom purrr map
+#' @importFrom dplyr as_tibble
 #' @examples
 #' \donttest{
 #' compare_data(emperors)
-#' compare_data(database = emperors, dataset = c("wikipedia", "UNRV"))
-#' compare_overlap(emperors, key = "ID")
-#' plot_overlap(emperors, key = "ID")
-#' compare_missing(emperors)
-#' plot_missing(emperors)
-#' compare_categories(emperors, key = "ID")
-#' compare_categories(database = emperors, dataset = c("wikipedia", "UNRV"),
-#' key = "ID", variable = c("Beg", "End"), category = c("conflict", "unique"))
-#' plot_categories(emperors, key = "ID")
-#' plot_categories(database = emperors, key = "ID", variable = c("Beg", "End"),
-#' category = c("conflict", "unique"))
 #' }
 #' @return
-#' The compare functions return tibbles with the respective comparative
-#' information, or the same information plotted appropriately.
-NULL
-
-#' @describeIn compare Compare data summaries for 'many' data
-#' @details `compare_data()` compares the number of observations, variables,
-#' the earliest and latest date in each dataset in a 'many' database.
-#' @importFrom purrr map
+#' `compare_data()` returns a tibble with information about each dataset
+#' including the number of observations, the number of variables,
+#' the earliest date, and the latest date in all observations.
 #' @export
 compare_data <- function(database, dataset = NULL) {
   if (!is.null(dataset)) {
@@ -71,54 +38,95 @@ compare_data <- function(database, dataset = NULL) {
     })), na.rm = TRUE))
     cbind(Observations, Variables, Earliest_Date, Latest_Date)
   }))
-  out <- cbind(names, out)
-  dplyr::as_tibble(out)
+  dplyr::as_tibble(cbind(names, out))
 }
 
-#' @describeIn compare Compare the overlap between datasets in 'many' databases
+#' Compare the overlap between datasets in 'many' databases
+#' 
 #' @details `compare_overlap()` compares the overlap between "key" observations
 #' in each dataset in a 'many' database.
-#' "manyID" by default.
-#' @importFrom dplyr select rename
+#' @family compare_
+#' @param database A database from one of the many packages.
+#' @param dataset A dataset in a database from one of the many packages.
+#' NULL by default.
+#' That is, all datasets in the database are used.
+#' A list of 2 or more datasets present in the database.
+#' @param key A variable key to join datasets.
+#' 'manyID' by default.
+#' @importFrom dplyr select rename_with as_tibble
+#' @importFrom purrr map
+#' @examples
+#' \donttest{
+#' compare_overlap(emperors, key = "ID")
+#' plot(compare_overlap(emperors, key = "ID"))
+#' }
+#' @return
+#' `compare_overlap()` returns a tibble with information about each dataset
+#' and the number of overlapping observations.
 #' @export
 compare_overlap <- function(database, dataset = NULL, key = "manyID") {
-  name <- NULL
+  name <- db_name <- NULL
   thisRequires("ggVennDiagram")
   if (!is.null(dataset)) {
     if (length(dataset) < 2) stop("Please declare 2 or more datasets for comparison.")
     database <- database[grepl(paste(dataset, collapse = "|"), names(database))]
   }
+  db_name <- deparse(substitute(database))
   out <- purrr::map(database, key)
   out <- ggVennDiagram::Venn(out)
   out <- ggVennDiagram::process_data(out)
   out <- ggVennDiagram::venn_region(out)
-  dplyr::as_tibble(out) %>%
-    dplyr::select(name, count) %>% 
-    dplyr::rename(Dataset = name, 'Overlapping Observations' = count)
+  out <- dplyr::as_tibble(out) %>%
+    dplyr::select(name, count) %>%
+    dplyr::rename_with(.fn = ~paste0("Datasets from ", db_name),
+                       .cols = name) %>%
+    dplyr::rename_with(.fn = ~paste0("Overlapping Observations by ", key),
+                       .cols = count)
+  class(out) <- c("compare_overlap", "tbl_df", "tbl", "data.frame")
+  out
 }
 
-#' @describeIn compare Plots the overlap between datasets in 'many' databases
-#' @details `plot_overlap()` plots the overlap between "key" observations
-#' in each dataset in a 'many' database.
+#' @importFrom stringr word
+#' @importFrom purrr map
 #' @export
-plot_overlap <- function(database, dataset = NULL, key = "manyID") {
+plot.compare_overlap <- function(x, ...) {
   thisRequires("ggVennDiagram")
-  if (!is.null(dataset)) {
-    if (length(dataset) < 2)
-      stop("Please declare 2 or more datasets for comparison.")
-    if (length(dataset) > 4)
-      message("Too many datasets can make the Venn diagram hard to interpret.")
+  database <- get(stringr::word(names(x[1]),-1))
+  dataset <- grep("\\.\\.", unique(unname(unlist(x[,1]))),
+                  value = TRUE, invert = TRUE)
+  key <- stringr::word(names(x[2]),-1)
+  if (length(names(database)) != length(dataset)) {
     database <- database[grepl(paste(dataset, collapse = "|"), names(database))]
   }
   out <- purrr::map(database, key)
   ggVennDiagram::ggVennDiagram(out)
 }
 
-#' @describeIn compare Compare missing observations for 'many' data
+#' Compare missing observations for 'many' data
+#' 
 #' @details `compare_missing()` compares the missing observations for variables
 #' in each dataset in a 'many' database.
+#' @family compare_
 #' @importFrom purrr map
-#' @importFrom dplyr arrange
+#' @importFrom dplyr arrange mutate as_tibble
+#' @param database A database from one of the many packages.
+#' @param dataset A dataset in a database from one of the many packages.
+#' NULL by default.
+#' That is, all datasets in the database are used.
+#' A list of 2 or more datasets present in the database.
+#' @param variable Would you like to focus on one, or more, specific variables
+#' present in one or more datasets in the 'many' database?
+#' By default "all".
+#' For multiple variables, please declare variable names as a vector.
+#' @examples
+#' \donttest{
+#' compare_missing(emperors)
+#' plot(compare_missing(emperors))
+#' }
+#' @return
+#' `compare_missing()` returns a tibble with information about each dataset
+#' including the number of observations, the number of variables,
+#' the earliest date, and the latest date in all observations.
 #' @export
 compare_missing <- function(database, dataset = NULL, variable = "all") {
   # Avoid notes
@@ -153,25 +161,21 @@ compare_missing <- function(database, dataset = NULL, variable = "all") {
     cbind(Variable = varnames, Dataset = dataset, Class = datatype,
           Count = counts, Missing = mvalues, 'Percent Missing' = mvaluesper)
   }))
-  dplyr::as_tibble(out) %>%
+  out <- dplyr::as_tibble(out) %>%
     dplyr::mutate(Count = as.numeric(Count),
                   Missing = as.numeric(Missing),
                   'Percent Missing' = as.numeric(`Percent Missing`)) %>%
     dplyr::arrange(Variable)
+  class(out) <- c("compare_missing", "tbl_df", "tbl", "data.frame")
+  out
 }
 
-#' @describeIn compare Plots missing observations for 'many' data
-#' @details `plot_missing()` plots the missing observations for variables
-#' in each dataset in a 'many' database.
 #' @import ggplot2
 #' @export
-#' @export
-plot_missing <- function(database, dataset = NULL, variable = "all") {
+plot.compare_missing <- function(x, ...) {
   'Percent Missing' <- Variable <- Dataset <- NULL
-  out <- compare_missing(database = database, dataset = dataset,
-                         variable = variable)
   # Plot missing proportions for variables
-  ggplot(out, aes(reorder(Dataset, `Percent Missing`, decreasing = TRUE),
+  ggplot(x, aes(reorder(Dataset, `Percent Missing`, decreasing = TRUE),
                   reorder(Variable, `Percent Missing`))) +
     geom_tile(aes(fill = `Percent Missing`)) +
     scale_fill_gradient(low = "darkgreen", high = "red3", na.value = NA,
@@ -181,19 +185,44 @@ plot_missing <- function(database, dataset = NULL, variable = "all") {
     labs(x = "Database", y = "Variable")
 }
 
-#' @describeIn compare Compare categories in 'many' databases
+#' Compare categories in 'many' databases
+#' 
 #' @details Confirmed values are the same in all datasets in database.
 #' Unique values appear once in datasets in database.
 #' Missing values are missing in all datasets in database.
 #' Conflict values are different in the same number of datasets in database.
 #' Majority values have the same value in multiple, but not all,
 #' datasets in database.
+#' @family compare_
+#' @param database A database from one of the many packages.
+#' @param dataset A dataset in a database from one of the many packages.
+#' NULL by default.
+#' That is, all datasets in the database are used.
+#' A list of 2 or more datasets present in the database.
+#' @param key A variable key to join datasets.
+#' 'manyID' by default.
+#' @param variable Would you like to focus on one, or more, specific variables
+#' present in one or more datasets in the 'many' database?
+#' By default "all".
+#' For multiple variables, please declare variable names as a vector.
+#' @param category Would you like to focus on one specific code category?
+#' By default "all" are returned.
+#' Other options include "confirmed", "unique", "missing", "conflict",
+#' or "majority".
+#' For multiple variables, please declare categories as a vector.
 #' @importFrom dplyr full_join filter_all %>% all_of group_by distinct any_vars
-#' starts_with mutate
+#' starts_with mutate tibble
 #' @importFrom purrr reduce map
 #' @importFrom tidyr drop_na
 #' @importFrom stringr str_count str_remove_all str_split str_extract_all
 #' str_replace_all
+#' @examples
+#' compare_categories(emperors, key = "ID")
+#' compare_categories(database = emperors, dataset = c("wikipedia", "UNRV"),
+#' key = "ID", variable = c("Beg", "End"), category = c("conflict", "unique"))
+#' plot(compare_categories(emperors, key = "ID"))
+#' plot(compare_categories(database = emperors, dataset = c("wikipedia", "UNRV"),
+#' key = "ID", variable = c("Beg", "End"), category = c("conflict", "unique")))
 #' @export
 compare_categories <- function(database,
                                dataset = NULL,
@@ -308,36 +337,23 @@ compare_categories <- function(database,
                             dplyr::any_vars(grepl(paste(category,
                                                         collapse = "|"), .)))
   }
+  class(db) <- c("compare_categories", "tbl_df", "tbl", "data.frame")
   db
 }
 
-#' @describeIn compare Plots categories in 'many' databases
-#' @details Confirmed values are the same in all datasets in database.
-#' Unique values appear once in datasets in database.
-#' Missing values are missing in all datasets in database.
-#' Conflict values are different in the same number of datasets in database.
-#' Majority values have the same value in multiple, but not all,
-#' datasets in database.
 #' @importFrom purrr map
-#' @importFrom dplyr summarise group_by mutate select %>% filter all_of
+#' @importFrom dplyr summarise group_by mutate select %>% filter
 #' @importFrom tidyr pivot_longer pivot_wider replace_na fill
 #' @importFrom stats reorder
 #' @import ggplot2
 #' @export
-plot_categories <- function(database,
-                               dataset = NULL,
-                               key = "manyID",
-                               variable = "all",
-                               category = "all") {
+plot.compare_categories <- function(x, ...) {
   Category <- Variable <- Percentage <- Missing <- NULL # to avoid notes
-  # Step 1: compare_categories(), get variable names, and code observations
-  db <- compare_categories(database = database, dataset = dataset,
-                           key = key, variable = variable, category = category)
-  # Step 2: remove extra variable level information
-  db <- db[!grepl("\\$", names(db))]
+  # Step 1: remove extra variable level information
+  db <- x[!grepl("\\$", names(x))]
+  
   # Step 3: gather and reshape the data
-  dbgather <- db %>%
-    dplyr::select(-all_of(key)) %>%
+  dbgather <- db[, -1] %>%
     tidyr::pivot_longer(cols = everything(), names_to = "Variable",
                         values_to = "Category") %>%
     dplyr::group_by(Variable, Category) %>%
