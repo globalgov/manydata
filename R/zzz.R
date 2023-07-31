978ab39ea63fa2b3bff20c6d1b38d10eee0c9bad
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-07-31 13:52:06 +0200

    Added hyperlink to package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach <- function(lib, pkg) {
   msg <- c(paste0("manydata ", utils::packageVersion("manydata")),
-           "\nFor more information about the package please visit https://manydata.ch",
+           "\nFor more information about the package please visit ",
+           cli::style_hyperlink("http://manydata.ch", "http://manydata.ch"),
            "\nType 'citation(\"manydata\")' for citing this R package in publications.")
   packageStartupMessage(msg)      
   invisible()
