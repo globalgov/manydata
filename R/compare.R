a1fee327dc525ef567491f02188467b7c4403ce8
Jael Tan <jael.tan@graduateinstitute.ch>
2024-03-13 14:47:25 +0100

    Updated compare_dimensions to remove NAs before taking final maximum/minimum date

--- R/compare.R
+++ R/compare.R
@@ -32,10 +32,10 @@ compare_dimensions <- function(datacube, dataset = "all") {
     Observations <- nrow(x)
     Variables <- paste(names(x), collapse = ", ")
     Earliest_Date <- suppressWarnings(min(unlist(purrr::map(x, function(y) {
-      ifelse(class(y) == "mdate", min(min(y, na.rm = TRUE)), NA)
+      ifelse(class(y) == "mdate", min(min(y, na.rm = TRUE), na.rm = TRUE), NA)
     })), na.rm = TRUE))
     Latest_Date <- suppressWarnings(max(unlist(purrr::map(x, function(y) {
-      ifelse(class(y) == "mdate", max(max(y, na.rm = TRUE)), NA)
+      ifelse(class(y) == "mdate", max(max(y, na.rm = TRUE), na.rm = TRUE), NA)
     })), na.rm = TRUE))
     cbind(Observations, Variables, Earliest_Date, Latest_Date)
   }))
