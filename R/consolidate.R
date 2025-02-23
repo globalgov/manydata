7c708d6ebebd890341c98d56f6a62d88307003a3
James Hollway <james.hollway@graduateinstitute.ch>
2025-02-23 21:12:38 +0100

    Updated consolidate documentation

--- R/consolidate.R
+++ R/consolidate.R
@@ -1,55 +1,56 @@
 #' Consolidate datacube into a single dataset
-#'
-#' This function consolidates a set of datasets in a 'many* package' datacube
-#' into a single dataset with some combination of the rows, columns,
-#' and observations of the datasets in the datacube.
-#' The function includes separate arguments for the rows and columns,
-#' as well as for how to resolve conflicts for observations across datasets.
-#' This provides users with considerable flexibility in how they combine data.
-#' For example, users may wish to stick to units that appear in
-#' every dataset but include variables coded in any dataset,
-#' or units that appear in any dataset
-#' but only those variables that appear in every dataset.
-#' Even then there may be conflicts, as the actual unit-variable
-#' observations may differ from dataset to dataset.
-#' We offer a number of resolve methods that enable
-#' users to choose how conflicts between observations are resolved.
+#' @description
+#'   This function consolidates a set of datasets in a 'many*' package datacube
+#'   into a single dataset with some combination of the rows, columns,
+#'   and observations of the datasets in the datacube.
+#' @details
+#'   The function includes separate arguments for the rows and columns,
+#'   as well as for how to resolve conflicts for observations across datasets.
+#'   This provides users with considerable flexibility in how they combine data.
+#'   For example, users may wish to stick to units that appear in
+#'   every dataset but include variables coded in any dataset,
+#'   or units that appear in any dataset
+#'   but only those variables that appear in every dataset.
+#'   Even then there may be conflicts, as the actual unit-variable
+#'   observations may differ from dataset to dataset.
+#'   We offer a number of resolve methods that enable
+#'   users to choose how conflicts between observations are resolved.
 #' @param datacube A datacube from one of the many packages
 #' @param rows Which rows or units to retain.
-#' By default "any" (or all) units are retained,
-#' but another option is "every",
-#' which retains only those units that appear in all parent datasets.
+#'   By default "any" (or all) units are retained,
+#'   but another option is "every",
+#'   which retains only those units that appear in all parent datasets.
 #' @param cols Which columns or variables to retain.
-#' By default "any" (or all) variables are retained,
-#' but another option is "every",
-#' which retains only those variables that appear
-#' in all parent datasets.
+#'   By default "any" (or all) variables are retained,
+#'   but another option is "every",
+#'   which retains only those variables that appear
+#'   in all parent datasets.
 #' @param resolve How should conflicts between observations be resolved?
-#' By default "coalesce",
-#' but other options include: "min", "max", "mean", "median", and "random".
-#' "coalesce" takes the first non-NA value.
-#' "max" takes the largest value.
-#' "min" takes the smallest value.
-#' "mean" takes the average value.
-#' "median" takes the median value.
-#' "random" takes a random value.
-#' For different variables to be resolved differently,
-#' you can specify the variables' names alongside
-#' how each is to be resolved in a list
-#' (e.g. `resolve = c(var1 = "min", var2 = "max")`).
-#' In this case, only the variables named will be resolved and returned.
+#'   By default "coalesce",
+#'   but other options include: "min", "max", "mean", "median", and "random".
+#'   "coalesce" takes the first non-NA value.
+#'   "max" takes the largest value.
+#'   "min" takes the smallest value.
+#'   "mean" takes the average value.
+#'   "median" takes the median value.
+#'   "random" takes a random value.
+#'   For different variables to be resolved differently,
+#'   you can specify the variables' names alongside
+#'   how each is to be resolved in a list
+#'   (e.g. `resolve = c(var1 = "min", var2 = "max")`).
+#'   In this case, only the variables named will be resolved and returned.
 #' @param key An ID column to collapse by.
-#' By default "manyID".
-#' Users can also specify multiple key variables in a list.
-#' For multiple key variables, the key variables must be present in
-#' all the datasets in the datacube (e.g. `key = c("key1", "key2")`).
-#' For equivalent key columns with different names across datasets,
-#' matching is possible if keys are declared (e.g. `key = c("key1" = "key2")`).
-#' Missing observations in the key variable are removed.
+#'   By default "manyID".
+#'   Users can also specify multiple key variables in a list.
+#'   For multiple key variables, the key variables must be present in
+#'   all the datasets in the datacube (e.g. `key = c("key1", "key2")`).
+#'   For equivalent key columns with different names across datasets,
+#'   matching is possible if keys are declared (e.g. `key = c("key1" = "key2")`).
+#'   Missing observations in the key variable are removed.
 #' @details Text variables are dropped for more efficient consolidation.
 #' @importFrom purrr reduce map pluck
 #' @importFrom dplyr select full_join inner_join distinct all_of
-#' group_by %>% mutate_at as_tibble
+#' @importFrom dplyr group_by %>% mutate_at as_tibble
 #' @importFrom tidyr drop_na
 #' @importFrom plyr ddply
 #' @importFrom zoo na.locf
